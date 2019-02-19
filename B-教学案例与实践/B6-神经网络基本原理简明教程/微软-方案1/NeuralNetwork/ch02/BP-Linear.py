ed77fe098b1817922552bbce8e0a8acab6272bc8
Xiaowu Hu <xiaowuhu@microsoft.com>
2019-02-19 18:02:03 +0800

    update

--- B-教学案例与实践/B6-神经网络基本原理简明教程/微软-方案1/NeuralNetwork/ch02/BP-Linear.py
+++ B-教学案例与实践/B6-神经网络基本原理简明教程/微软-方案1/NeuralNetwork/ch02/BP-Linear.py
@@ -7,16 +7,16 @@ def target_function(w,b):
     x = 2*w+3*b
     y=2*b+1
     z=x*y
-    return z
+    return x,y,z
 
 def single_variable(w,b,t):
     print("single variable: b ----- ")
     error = 1e-5
     while(True):
-        z = target_function(w,b)
+        x,y,z = target_function(w,b)
         delta_z = z - t
         print("w=%f,b=%f,z=%f,delta_z=%f"%(w,b,z,delta_z))
-        if delta_z < error:
+        if abs(delta_z) < error:
             break
         delta_b = delta_z /63
         print("delta_b=%f"%delta_b)
@@ -29,10 +29,10 @@ def double_variable(w,b,t):
     print("double variable: w, b -----")
     error = 1e-5
     while(True):
-        z = target_function(w,b)
+        x,y,z = target_function(w,b)
         delta_z = z - t
         print("w=%f,b=%f,z=%f,delta_z=%f"%(w,b,z,delta_z))
-        if delta_z < error:
+        if abs(delta_z) < error:
             break
         delta_b = delta_z /63/2
         delta_w = delta_z/18/2
@@ -44,9 +44,35 @@ def double_variable(w,b,t):
     print("final w=%f"%w)
 
 
+def double_variable_new(w,b,t):
+    print("double variable: w, b -----")
+    error = 1e-5
+    while(True):
+        x,y,z = target_function(w,b)
+        delta_z = z - t
+        print("w=%f,b=%f,z=%f,delta_z=%f"%(w,b,z,delta_z))
+        if abs(delta_z) < error:
+            break
+
+        factor_b, factor_w = calculate_wb_factor(x,y)
+        delta_b = delta_z/factor_b/2
+        delta_w = delta_z/factor_w/2
+        print("factor_b=%f, factor_w=%f, delta_b=%f, delta_w=%f"%(factor_b, factor_w, delta_b,delta_w))
+        b = b - delta_b
+        w = w - delta_w
+    print("done!")
+    print("final b=%f"%b)
+    print("final w=%f"%w)
+
+def calculate_wb_factor(x,y):
+    factor_b = 2*x+3*y
+    factor_w = 2*y
+    return factor_b, factor_w
+
 if __name__ == '__main__':
-    w = 3
-    b = 4
-    t = 100
+    w = 2
+    b = 2
+    t = 150
     single_variable(w,b,t)
     double_variable(w,b,t)
+    double_variable_new(w,b,t)

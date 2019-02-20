607473a3a347f9ce2b9657a8ed07a656f4c5b3d2
xiaowuhu <hu.xiaowu@hotmail.com>
2019-02-20 11:35:18 +0800

    Xiaowu/20190213 (#110)
    * update
    
    * update
    
    * Update 2-反向传播与梯度下降.md
    
    * Update NN_Images.vsdx
    
    * update
    
    * update
    
    * update
    
    * Update 1-神经网络的基本工作原理.md
    
    * Update 1-神经网络的基本工作原理.md
    
    * Update 1-神经网络的基本工作原理.md
    
    * update
    
    * update
    
    * update
    
    * Update 3-损失函数.md
    
    * update
    
    * update
    
    * update
    
    * Update 1-神经网络的基本工作原理.md
    
    * update

---
+++ B-教学案例与实践/B6-神经网络基本原理简明教程/微软-方案1/NeuralNetwork/ch02/BP-NoneLinear.py
@@ -0,0 +1,55 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def draw_fun():
+    x = np.linspace(1,10)
+    a = x*x
+    b = np.log(a)
+    c = np.sqrt(b)
+    plt.plot(x,c)
+    #plt.show()
+
+    d = 1/(x*np.sqrt(np.log(x)))
+    plt.plot(x,d)
+    plt.show()
+
+
+if __name__ == '__main__':
+    print("how to play: 1) input x, 2) calculate c, 3) input target number but not faraway from c")
+    print("input x as initial number:")
+    line = input()
+    x = float(line)
+    a = x*x
+    b = np.log(a)
+    c = np.sqrt(b)
+    print("c=%f" %c)
+    print("input y as target number(not farway from c):")
+    line = input()
+    y = float(line)
+    while(True):
+        print("forward...")
+        # forward
+        a = x*x
+        b = np.log(a)
+        c = np.sqrt(b)
+        print(x,a,b,c)
+        print("backward...")
+        # backward
+        loss = c - y
+        if np.abs(loss) < 0.001:
+            break
+        delta_c = loss
+        delta_b = delta_c * 2 * np.sqrt(b)
+        delta_a = delta_b * a
+        delta_x = delta_a / 2 / x
+        x = x - delta_x
+        print(delta_c, delta_b, delta_a, delta_x)
+
+    print(x,a,b,c,loss)
+    draw_fun()
+
+
+

68b7eebd482af10f1313fde902ebedf3687f227a
xiaowuhu <hu.xiaowu@hotmail.com>
2019-03-11 17:08:40 +0800

    Xiaowuhu/603 (#130)
    * update
    
    * update
    
    * update

--- B-教学案例与实践/B6-神经网络基本原理简明教程/微软-方案1/NeuralNetwork/ch07/level1_BinaryClassification.py
+++ B-教学案例与实践/B6-神经网络基本原理简明教程/微软-方案1/NeuralNetwork/ch07/level1_BinaryClassification.py
@@ -10,16 +10,6 @@ from BaseClassification import *
 x_data_name = "X2.dat"
 y_data_name = "Y2.dat"
 
-def ShowData(X,Y):
-    for i in range(X.shape[1]):
-        if Y[0,i] == 0:
-            plt.plot(X[0,i], X[1,i], '.', c='r')
-        elif Y[0,i] == 1:
-            plt.plot(X[0,i], X[1,i], 'x', c='g')
-        # end if
-    # end for
-    plt.show()
-
 def ToBool(YData):
     num_example = YData.shape[1]
     Y = np.zeros((1, num_example))
@@ -58,31 +48,6 @@ def CheckLoss(W, B, X, Y):
     loss = LOSS / m
     return loss
 
-
-def ShowResult(X,Y,W,B,xt):
-    for i in range(X.shape[1]):
-        if Y[0,i] == 0:
-            plt.plot(X[0,i], X[1,i], '.', c='r')
-        elif Y[0,i] == 1:
-            plt.plot(X[0,i], X[1,i], 'x', c='g')
-        # end if
-    # end for
-
-    b12 = -B[0,0]/W[0,1]
-    w12 = -W[0,0]/W[0,1]
-
-    x = np.linspace(0,1,10)
-    y = w12 * x + b12
-    plt.plot(x,y)
-
-    for i in range(xt.shape[1]):
-        plt.plot(xt[0,i], xt[1,i], 'x')
-
-#    title = str.format("eta:{0}, iteration:{1}, eps:{2}", eta, iteration, eps)
-#    plt.title(title)
-    plt.axis([-0.1,1.1,-0.1,1.1])
-    plt.show()
-
 def Inference(W,B,X_norm,xt):
     xt_normalized = NormalizePredicateData(xt, X_norm)
     A = ForwardCalculationBatch(W,B,xt_normalized)
@@ -95,15 +60,15 @@ if __name__ == '__main__':
     # read data
     XData,YData = ReadData(x_data_name, y_data_name)
     X, X_norm = NormalizeData(XData)
-    ShowData(XData, YData)
     Y = ToBool(YData)
     W, B = train(method, X, Y, ForwardCalculationBatch, CheckLoss)
     print("W=",W)
     print("B=",B)
-    xt = np.array([5.1,12.7,25.3,42.1,34.3,82.2]).reshape(2,3,order='F')
+    xt = np.array([5,1,6,9,5,5]).reshape(2,3,order='F')
     result, xt_norm = Inference(W,B,X_norm,xt)
-    print(result)
-    ShowResult(X,YData,W,B,xt_norm)
+    print("result=", result)
+    print(np.around(result))
+
 
 
 
--- B-教学案例与实践/B6-神经网络基本原理简明教程/微软-方案1/NeuralNetwork/ch07/level4_ShowMultipleResult.py
+++ B-教学案例与实践/B6-神经网络基本原理简明教程/微软-方案1/NeuralNetwork/ch07/level4_ShowMultipleResult.py
@@ -6,9 +6,7 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 import math
 from BaseClassification import *
-
-x_data_name = "X3.dat"
-y_data_name = "Y3.dat"
+from level3_MultipleClassification import *
 
 def ShowData(X,Y):
     for i in range(X.shape[1]):
@@ -22,48 +20,6 @@ def ShowData(X,Y):
     # end for
     plt.show()
 
-def ToOneHot(YData, num_category):
-    num_example = YData.shape[1]
-    Y = np.zeros((num_category, num_example))
-    for i in range(num_example):
-        if YData[0,i] == 1:
-            Y[0,i] = 1
-        elif YData[0,i] == 2:
-            Y[1,i] = 1
-        elif YData[0,i] == 3:
-            Y[2,i] = 1
-        # end if
-    # end for
-    return Y
-
-def Softmax(Z):
-    shift_z = Z - np.max(Z, axis=0)
-    exp_z = np.exp(shift_z)
-    A = exp_z / np.sum(exp_z, axis=0)
-    return A
-
-# 前向计算
-def ForwardCalculationBatch(W, B, batch_X):
-    Z = np.dot(W, batch_X) + B
-    A = Softmax(Z)
-    return A
-
-# 计算损失函数值
-def CheckLoss(W, B, X, Y):
-    m = X.shape[1]
-    A = ForwardCalculationBatch(W,B,X)
-    p1 = np.log(A)
-    p2 =  np.multiply(Y, p1)
-    LOSS = np.sum(-p2) 
-    loss = LOSS / m
-    return loss
-
-def Inference(W,B,X_norm,xt):
-    xt_normalized = NormalizePredicateData(xt, X_norm)
-    A = ForwardCalculationBatch(W,B,xt_normalized)
-    r = np.argmax(A,axis=0)+1
-    return A, xt_normalized, r
-
 def ShowResult(X,Y,W,B,xt):
     for i in range(X.shape[1]):
         if Y[0,i] == 1:
@@ -75,14 +31,14 @@ def ShowResult(X,Y,W,B,xt):
         # end if
     # end for
 
-    b13 = (B[0,0] - B[2,0])/(W[2,1] - W[0,1])
-    w13 = (W[0,0] - W[2,0])/(W[2,1] - W[0,1])
+    b12 = (B[1,0] - B[0,0])/(W[0,1] - W[1,1])
+    w12 = (W[1,0] - W[0,0])/(W[0,1] - W[1,1])
 
     b23 = (B[2,0] - B[1,0])/(W[1,1] - W[2,1])
     w23 = (W[2,0] - W[1,0])/(W[1,1] - W[2,1])
 
-    b12 = (B[1,0] - B[0,0])/(W[0,1] - W[1,1])
-    w12 = (W[1,0] - W[0,0])/(W[0,1] - W[1,1])
+    b13 = (B[2,0] - B[0,0])/(W[0,1] - W[2,1])
+    w13 = (W[2,0] - W[0,0])/(W[0,1] - W[2,1])
 
     x = np.linspace(0,1,2)
     y = w13 * x + b13
@@ -97,9 +53,6 @@ def ShowResult(X,Y,W,B,xt):
     p12, = plt.plot(x,y)
 
     plt.legend([p13,p23,p12], ["13","23","12"])
-
-#    title = str.format("eta:{0}, iteration:{1}, eps:{2}", eta, iteration, eps)
-#    plt.title(title)
     
     for i in range(xt.shape[1]):
         plt.plot(xt[0,i], xt[1,i], 'o')
@@ -107,13 +60,15 @@ def ShowResult(X,Y,W,B,xt):
     plt.axis([-0.1,1.1,-0.1,1.1])
     plt.show()
 
+
 # 主程序
 if __name__ == '__main__':
     # SGD, MiniBatch, FullBatch
-    method = "SGD"
+    method = "FullBatch"
     # read data
     XData,YData = ReadData(x_data_name, y_data_name)
     X, X_norm = NormalizeData(XData)
+    ShowData(XData, YData)
     num_category = 3
     Y = ToOneHot(YData, num_category)
     W, B = train(method, X, Y, ForwardCalculationBatch, CheckLoss)
@@ -124,5 +79,6 @@ if __name__ == '__main__':
     a, xt_norm, r = Inference(W,B,X_norm,xt)
     print("Probility=", a)
     print("Result=",r)
+    ShowResult(X,YData,W,B,xt_norm)
 
 

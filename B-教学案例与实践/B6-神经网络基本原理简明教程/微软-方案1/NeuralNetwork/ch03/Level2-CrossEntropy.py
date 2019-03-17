6ea3072070c902f404ac5d6072861d07d22c3330
Xiaowu Hu <xiaowuhu@microsoft.com>
2019-03-17 14:18:15 +0800

    update

--- B-教学案例与实践/B6-神经网络基本原理简明教程/微软-方案1/NeuralNetwork/ch03/Level2-CrossEntropy.py
+++ B-教学案例与实践/B6-神经网络基本原理简明教程/微软-方案1/NeuralNetwork/ch03/Level2-CrossEntropy.py
@@ -4,16 +4,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def target_function(x):
-    p1 = x * np.log(x)
-    p2 = (1-x) * np.log(1-x)
+def target_function2(a,y):
+    p1 = y * np.log(a)
+    p2 = (1-y) * np.log(1-a)
     y = -p1 - p2
     return y
 
 if __name__ == '__main__':
-    err = 1e-5  # avoid invalid math caculation
-    x = np.linspace(0+err,1-err)
-    y = target_function(x)
-    plt.plot(x,y)
+    err = 1e-2  # avoid invalid math caculation
+    a = np.linspace(0+err,1-err)
+    y = 0
+    z1 = target_function2(a,y)
+    y = 1
+    z2 = target_function2(a,y)
+    p1, = plt.plot(a,z1)
+    p2, = plt.plot(a,z2)
     plt.grid()
+    plt.legend([p1,p2],["y=0","y=1"])
+    plt.xlabel("a")
+    plt.ylabel("Loss")
     plt.show()

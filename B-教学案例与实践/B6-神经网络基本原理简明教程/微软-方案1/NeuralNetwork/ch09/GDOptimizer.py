274f69f4f059e41a6b87a128b8e99f55b7fe4f91
Xiaowu Hu <xiaowuhu@microsoft.com>
2019-03-23 22:37:18 +0800

    update

--- B-教学案例与实践/B6-神经网络基本原理简明教程/微软-方案1/NeuralNetwork/ch09/GDOptimizer.py
+++ B-教学案例与实践/B6-神经网络基本原理简明教程/微软-方案1/NeuralNetwork/ch09/GDOptimizer.py
@@ -16,7 +16,6 @@ class SGD(GDOptimizer):
         theta = theta - self.eta * grad
         return theta
 
-
 class Momentum(GDOptimizer):
     def __init__(self, eta):
         self.vt = 0
@@ -24,9 +23,33 @@ class Momentum(GDOptimizer):
         self.alpha = 0.9
 
     def update(self, theta, grad):
-        vt_new = self.eta * grad - self.alpha * self.vt
+        vt_new = self.alpha * self.vt + self.eta * grad
         theta = theta - vt_new
         self.vt = vt_new
         return theta
 
-    
+class AdaGrad(GDOptimizer):
+    def __init__(self, eta):
+        self.delta = 1e-7
+        self.eta = eta
+        self.r = 0
+
+    def step(self, theta, grad):
+        self.r = self.r + np.multiply(grad, grad)
+        alpha = self.eta / (np.sqrt(self.r) + self.delta)
+        theta = theta - alpha * grad
+        return theta
+  
+class CRMSProp(GDOptimizer):
+    def __init__(self, eta):
+        self.eta = eta
+        self.p = 0.9
+        self.delta = 1e-6
+        self.r = 0
+
+    def step(self, theta, grad):
+        grad2 = np.multiply(grad, grad)
+        self.r = self.p * self.r + (1-self.p) * grad2
+        alpha = self.eta / np.sqrt(self.delta + self.r)
+        theta = theta - np.multiply(alpha, grad)
+        return theta

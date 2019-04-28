608e78886ffe16dcb210d0a42923518d51e66dc1
Xiaowu Hu <xiaowuhu@microsoft.com>
2019-04-28 22:15:48 +0800

    fi'x

--- B-教学案例与实践/B6-神经网络基本原理简明教程/微软-方案1/NeuralNetwork/ch14-CNN/Cifar10.py
+++ B-教学案例与实践/B6-神经网络基本原理简明教程/微软-方案1/NeuralNetwork/ch14-CNN/Cifar10.py
@@ -5,12 +5,14 @@
 
 import pickle
 
+from CifarImageReader import *
 
-def unpickle(file):
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
+file_1 = "C:\\MyDocument\\cifar-10-batches-bin\\data_batch_1.bin"
+file_2 = "C:\\MyDocument\\cifar-10-batches-bin\\data_batch_2.bin"
+file_3 = "C:\\MyDocument\\cifar-10-batches-bin\\data_batch_3.bin"
+file_4 = "C:\\MyDocument\\cifar-10-batches-bin\\data_batch_4.bin"
+file_5 = "C:\\MyDocument\\cifar-10-batches-bin\\data_batch_5.bin"
 
 if __name__ == '__main__':
-    dict = unpickle("c:\\MyDocument\\cifar-10-batches-py\\data_batch_1")
-    print(dict)
+    dr = CifarImageReader(file_1, None, None, None, None, None)
+    dr.ReadImageFile(file_1)

f89079203bc1afb2359a00f6ffb6e1399de356b7
Xiaowu Hu <xiaowuhu@microsoft.com>
2019-05-04 12:23:48 +0800

    fix

--- B-教学案例与实践/B6-神经网络基本原理简明教程/微软-方案1/NeuralNetwork/ch14-DNN/Level1_InitializeWeights.py
+++ B-教学案例与实践/B6-神经网络基本原理简明教程/微软-方案1/NeuralNetwork/ch14-DNN/Level1_InitializeWeights.py
@@ -2,57 +2,59 @@
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import numpy as np
-from pathlib import Path
 import matplotlib.pyplot as plt
-import math
 
-from LossFunction import * 
-from Level0_TwoLayerNet import *
-from DataReader import * 
-from WeightsBias import *
-from GDOptimizer import *
-
-x_data_name = "X8.dat"
-y_data_name = "Y8.dat"
-
-def ShowResult(net, X, Y, title, wb1, wb2):
-    # draw train data
-    plt.plot(X[0,:], Y[0,:], '.', c='b')
-    # create and draw visualized validation data
-    TX = np.linspace(0,1,100).reshape(1,100)
-    dict_cache = net.forward(TX, wb1, wb2)
-    TY = dict_cache["Output"]
-    plt.plot(TX, TY, 'x', c='r')
-    plt.title(title)
-    plt.show()
+from MiniFramework.NeuralNet import *
+from MiniFramework.GDOptimizer import *
+from MiniFramework.LossFunction import *
+from MiniFramework.Parameters import *
+from MiniFramework.WeightsBias import *
+from MiniFramework.Activators import *
 
 
-def train(init_method):
-    dataReader = DataReader(x_data_name, y_data_name)
-    XData,YData = dataReader.ReadData()
-    X = dataReader.NormalizeX(passthrough=True)
-    Y = dataReader.NormalizeY()
-    
-    n_input, n_hidden, n_output = 1, 4, 1
-    eta, batch_size, max_epoch = 0.5, 10, 30000
-    eps = 0.001
+def net():
 
-    params = CParameters(n_input, n_hidden, n_output,
-                         eta, max_epoch, batch_size, eps, 
-                         init_method,
-                         OptimizerName.SGD)
+    max_epoch = 1
+    batch_size = 5
+    learning_rate = 0.02
+    eps = 0.01
+
+    params = CParameters(learning_rate, max_epoch, batch_size, eps,
+                        LossFunctionName.CrossEntropy3, 
+                        InitialMethod.Normal, 
+                        OptimizerName.SGD)
 
     loss_history = CLossHistory()
-    net = TwoLayerNet(NetType.Fitting)
-    net.train(dataReader, params, loss_history)
 
-    trace = loss_history.GetMinimalLossData()
-    print(trace.toString())
-    title = loss_history.ShowLossHistory(params)
-    ShowResult(net, X, YData, title, trace.wb1, trace.wb2)
+    net = NeuralNet(params)
 
-if __name__ == '__main__':    
-    #train(InitialMethod.Zero)
-    #train(InitialMethod.Normal)
-    train(InitialMethod.Xavier)
-    #train(InitialMethod.MSRA)
+    #num_hidden = [256,128,64,32,16]
+    num_hidden = [100,100,100,100,100]
+    count = len(num_hidden)
+    layers = []
+
+    for i in range(count-1):
+        fc = FcLayer(num_hidden[i], num_hidden[i+1], Sigmoid())
+        layers.append(fc)
+        net.add_layer(fc)
+
+    x = np.random.randn(1000, num_hidden[0]).T
+    #x = np.random.normal(size=num_hidden[0]).T
+
+    a = []
+    a.append(x)
+
+    for i in range(count):
+        f = layers[0].forward(a[i])
+        a.append(f)
+
+
+    for i in range(count):
+        plt.subplot(1, count, i+1)
+        plt.title(str(i+1) + "-layer")
+        plt.ylim(0,7000)
+        plt.hist(a[i].flatten(), 30, range=(0,1))
+    plt.show()
+
+if __name__ == '__main__':
+    net()

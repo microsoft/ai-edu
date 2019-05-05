67a91a3df7929b52ed6d7fab52229e43efb82ab4
Xiaowu Hu <xiaowuhu@microsoft.com>
2019-05-05 16:23:44 +0800

    update

--- B-教学案例与实践/B6-神经网络基本原理简明教程/微软-方案1/NeuralNetwork/ch04-SingleVariableLinearRegression/level4-BatchGradientDescent.py
+++ B-教学案例与实践/B6-神经网络基本原理简明教程/微软-方案1/NeuralNetwork/ch04-SingleVariableLinearRegression/level4-BatchGradientDescent.py
@@ -10,15 +10,48 @@ x_data_name = "X04.dat"
 y_data_name = "Y04.dat"
 
 
-class CData(object):
-    def __init__(self, loss, w, b, epoch, iteration):
-        self.loss = loss
-        self.w = w
-        self.b = b
-        self.epoch = epoch
-        self.iteration = iteration
+class CParameters(object):
+    def __init__(self, eta=0.1, max_epoch=1000, batch_size=5, eps = 0.1):
+        self.eta = eta
+        self.max_epoch = max_epoch
+        self.batch_size = batch_size
+        self.eps = eps
 
+    def toString(self):
+        title = str.format("bz:{0},eta:{1}", self.batch_size, self.eta)
+        return title
 
+# 帮助类，用于记录损失函数值极其对应的权重/迭代次数
+class CLossHistory(object):
+    def __init__(self):
+        # loss history
+        self.iteration = []
+        self.loss_history = []
+        self.w_history = []
+        self.b_history = []
+
+    def AddLossHistory(self, iteration, loss, w, b):
+        self.iteration.append(iteration)
+        self.loss_history.append(loss)
+        self.w_history.append(w)
+        self.b_history.append(b)
+
+    # 图形显示损失函数值历史记录
+    def ShowLossHistory(self, params, xmin=None, xmax=None, ymin=None, ymax=None):
+        plt.plot(self.iteration, self.loss_history)
+        title = params.toString()
+        plt.title(title)
+        plt.xlabel("iteration")
+        plt.ylabel("loss")
+        if xmin != None and ymin != None:
+            plt.axis([xmin, xmax, ymin, ymax])
+        plt.show()
+        return title
+
+    def GetLast(self):
+        count = len(self.loss_history)
+        return self.loss_history[count-1], self.w_history[count-1], self.b_history[count-1]
+# end class
 
 def ReadData():
     Xfile = Path(x_data_name)
@@ -29,6 +62,22 @@ def ReadData():
         return X.reshape(1,-1),Y.reshape(1,-1)
     else:
         return None,None
+
+def GetBatchSamples(X,Y,batch_size,iteration):
+    num_feature = X.shape[0]
+    start = iteration * batch_size
+    end = start + batch_size
+    batch_x = X[0:num_feature,start:end].reshape(num_feature,batch_size)
+    batch_y = Y[0,start:end].reshape(1,batch_size)
+    return batch_x, batch_y
+
+def Shuffle(X, Y):
+    seed = np.random.randint(0,100)
+    np.random.seed(seed)
+    XP = np.random.permutation(X.T)
+    np.random.seed(seed)
+    YP = np.random.permutation(Y.T)
+    return XP.T, YP.T
 
 def ForwardCalculationBatch(W,B,batch_x):
     Z = np.dot(W, batch_x) + B
@@ -84,47 +133,12 @@ def CheckLoss(W, B, X, Y):
     loss = LOSS.sum()/m/2
     return loss
 
-def GetBatchSamples(X,Y,batch_size,iteration):
-    num_feature = X.shape[0]
-    start = iteration * batch_size
-    end = start + batch_size
-    batch_x = X[0:num_feature,start:end].reshape(num_feature,batch_size)
-    batch_y = Y[0,start:end].reshape(1,batch_size)
-    return batch_x, batch_y
+def loss_2d(x,y,n,loss_history,batch_size):
 
-def Shuffle(X, Y):
-    seed = np.random.randint(0,100)
-    np.random.seed(seed)
-    XP = np.random.permutation(X.T)
-    np.random.seed(seed)
-    YP = np.random.permutation(Y.T)
-    return XP.T, YP.T
-
-def GetMinimalLossData(dict_loss):
-    key = sorted(dict_loss.keys())[0]
-    w = dict_loss[key].w
-    b = dict_loss[key].b
-    return w,b,dict_loss[key]
-
-def ShowLossHistory(dict_loss, method):
-    loss = []
-    for key in dict_loss:
-        loss.append(key)
-
-    #plt.plot(loss)
-    plt.plot(loss[30:800])
-    plt.title(method)
-    plt.xlabel("epoch")
-    plt.ylabel("loss")
-    plt.show()
-
-def loss_2d(x,y,n,dict_loss,method,cdata):
-
-    result_w = cdata.w[0,0]
-    result_b = cdata.b[0,0]
+    last_loss, result_w, result_b = loss_history.GetLast()
 
     # show contour of loss
-    s = 150
+    s = 100
     W = np.linspace(result_w-1,result_w+1,s)
     B = np.linspace(result_b-1,result_b+1,s)
     LOSS = np.zeros((s,s))
@@ -166,95 +180,69 @@ def loss_2d(x,y,n,dict_loss,method,cdata):
     # end while
 
     # show w,b trace
-    w_history = []
-    b_history = []
-    for key in dict_loss:
-        w = dict_loss[key].w[0,0]
-        b = dict_loss[key].b[0,0]
-        if w < result_w-1 or result_b-1 < 2:
-            continue
-        if key == cdata.loss:
-            break
-        # end if
-        w_history.append(w)
-        b_history.append(b)
-    # end for
+    w_history = loss_history.w_history
+    b_history = loss_history.b_history
     plt.plot(w_history,b_history)
 
     plt.xlabel("w")
     plt.ylabel("b")
-    title = str.format("Method={0}, Epoch={1}, Iteration={2}, Loss={3:.3f}, W={4:.3f}, B={5:.3f}", method, cdata.epoch, cdata.iteration, cdata.loss, cdata.w[0,0], cdata.b[0,0])
+    title = str.format("bz={0}, Epoch={1}, Loss={2:.3f}, W={3:.3f}, B={4:.3f}", batch_size, epoch, last_loss, result_w, result_b)
     plt.title(title)
+    plt.axis([result_w-1,result_w+1,result_b-1,result_b+1])
     plt.show()
 
-def InitializeHyperParameters(method):
-    if method=="SGD":
-        eta = 0.1
-        max_epoch = 50
-        batch_size = 1
-    elif method=="MiniBatch":
-        eta = 0.1
-        max_epoch = 50
-        batch_size = 10
-    elif method=="FullBatch":
-        eta = 0.5
-        max_epoch = 1000
-        batch_size = 200
-    return eta, max_epoch, batch_size
 
 if __name__ == '__main__':
     
-    # 修改method分别为下面三个参数，运行程序，对比不同的运行结果
-    # SGD, MiniBatch, FullBatch
-    method = "SGD"
-
-    eta, max_epoch,batch_size = InitializeHyperParameters(method)
-    
+    # 修改params分别为下面三行，运行程序，对比不同的运行结果
+    #params = CParameters(eta=0.1, max_epoch=50, batch_size=1, eps = 0.005)
+    params = CParameters(eta=0.1, max_epoch=50, batch_size=10, eps = 0.005)
+    #params = CParameters(eta=0.5, max_epoch=1000, batch_size=200, eps = 0.005)
+     
     W, B = InitialWeights(1,1,0)
     # calculate loss to decide the stop condition
-    loss = 5
-    dict_loss = {}
+    loss_history = CLossHistory()
     # read data
     X, Y = ReadData()
     # count of samples
     num_example = X.shape[1]
     num_feature = X.shape[0]
 
-
     # if num_example=200, batch_size=10, then iteration=200/10=20
-    max_iteration = (int)(num_example / batch_size)
-    for epoch in range(max_epoch):
+    max_iteration = (int)(num_example / params.batch_size)
+    for epoch in range(params.max_epoch):
         print("epoch=%d" %epoch)
+        X,Y = Shuffle(X,Y)
         for iteration in range(max_iteration):
             # get x and y value for one sample
-            batch_x, batch_y = GetBatchSamples(X,Y,batch_size,iteration)
+            batch_x, batch_y = GetBatchSamples(X,Y,params.batch_size,iteration)
             # get z from x,y
             batch_z = ForwardCalculationBatch(W, B, batch_x)
             # calculate gradient of w and b
             dW, dB = BackPropagationBatch(batch_x, batch_y, batch_z)
             # update w,b
-            W, B = UpdateWeights(W, B, dW, dB, eta)
-            
-            # calculate loss for this batch
-            loss = CheckLoss(W,B,X,Y)
-            print(epoch,iteration,loss,W,B)
-            prev_loss = loss
-
-            dict_loss[loss] = CData(loss, W, B, epoch, iteration)            
+            W, B = UpdateWeights(W, B, dW, dB, params.eta)
+            if iteration % 10 == 0:
+                loss = CheckLoss(W,B,X,Y)
+                print(epoch, iteration, loss, W, B)
+                loss_history.AddLossHistory(epoch*max_iteration+iteration, loss, W[0,0], B[0,0])
+                if loss < params.eps:
+                    break
+                #end if
+            #end if
+            if loss < params.eps:
+                break
         # end for
-        #X,Y = Shuffle(X,Y)
+        if loss < params.eps:
+            break
     # end for
-    ShowLossHistory(dict_loss, method)
-    w,b,cdata = GetMinimalLossData(dict_loss)
-    print(cdata.w, cdata.b)
-    print("epoch=%d, iteration=%d, loss=%f" %(cdata.epoch, cdata.iteration, cdata.loss))
-
+    loss_history.ShowLossHistory(params)
     #ShowResult(X, Y, W, B, epoch)
-    print(w,b)
+    print(W,B)
 
     x = 346/1000
-    result = ForwardCalculationBatch(w, b, x)
+    result = ForwardCalculationBatch(W, B, x)
     print(result)
     
-    loss_2d(X,Y,200,dict_loss,method,cdata)
+    loss_2d(X,Y,num_example,loss_history,params.batch_size)
 

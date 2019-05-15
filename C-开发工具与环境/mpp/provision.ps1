7db1d5ea4596920c459cd7da0b6c815f9268b5f6
Bartuer Zhou <bazhou@microsoft.com>
2019-05-15 15:49:44 +0800

    mpp remove revoke-command

--- C-开发工具与环境/mpp/provision.ps1
+++ C-开发工具与环境/mpp/provision.ps1
@@ -1,6 +1,3 @@
-$sw = [Diagnostics.Stopwatch]::StartNew()
-$name="training.provision.$(get-random)"
-invoke-command -scriptblock {
     Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
     choco install docker-for-windows git python3 googlechrome vscode pycharm -y
     $h=hostname
@@ -18,9 +15,3 @@ invoke-command -scriptblock {
     invoke-webrequest https://raw.githubusercontent.com/microsoft/ai-edu/master/C-%E5%BC%80%E5%8F%91%E5%B7%A5%E5%85%B7%E4%B8%8E%E7%8E%AF%E5%A2%83/mpp/run.ps1 C:\training\run.ps1
     Write-Host "configure docker volume, share C folder, then restart machine"
     Write-Host "after rebooting, run.ps1"
-} -ThrottleLimit 16 -AsJob -jobname $name
-while (get-job -name $name|where-object {$_.state -eq "Running"}){}
-receive-job -name $name
-remove-job -name $name
-$sw.Stop()
-$sw.Elapsed

6de8eaa1c1ce01145e006fc54fbf784dc29f1a60
Bartuer Zhou <bazhou@microsoft.com>
2019-05-15 16:49:49 +0800

    mpp provision prompt

--- C-开发工具与环境/mpp/provision.ps1
+++ C-开发工具与环境/mpp/provision.ps1
@@ -39,9 +39,11 @@ python -m pip install matplotlib
 mkdir C:\training
 cd C:\traning
 invoke-webrequest https://raw.githubusercontent.com/microsoft/ai-edu/mpp/C-%E5%BC%80%E5%8F%91%E5%B7%A5%E5%85%B7%E4%B8%8E%E7%8E%AF%E5%A2%83/mpp/run.ps1 -outfile C:\training\run.ps1
-git clone https://github.com/bartuer/training_notebook.git
-cd training_notebook
-git pull
-cd ..
-Write-Host "configure docker volume, share C folder, then restart machine"
-Write-Host "after rebooting, run.ps1"
+try {
+    git clone https://github.com/bartuer/training_notebook.git
+} catch {
+    cd C:\training\training_notebook
+    git pull
+}
+Write-Host -ForegroundColor Yellow "configure docker volume, share C folder, then restart machine"
+Write-Host -ForegroundColor Yellow "after rebooting, cd C:\training .\run.ps1"

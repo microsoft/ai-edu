18a7066e263f6e92eb0d533f6d8a9f8c9756639a
Bartuer Zhou <bazhou@microsoft.com>
2019-05-15 16:33:31 +0800

    mpp provision pull notebook

--- C-开发工具与环境/mpp/provision.ps1
+++ C-开发工具与环境/mpp/provision.ps1
@@ -40,5 +40,8 @@ mkdir C:\training
 cd C:\traning
 invoke-webrequest https://raw.githubusercontent.com/microsoft/ai-edu/mpp/C-%E5%BC%80%E5%8F%91%E5%B7%A5%E5%85%B7%E4%B8%8E%E7%8E%AF%E5%A2%83/mpp/run.ps1 -outfile C:\training\run.ps1
 git clone https://github.com/bartuer/training_notebook.git
+cd training_notebook
+git pull
+cd ..
 Write-Host "configure docker volume, share C folder, then restart machine"
 Write-Host "after rebooting, run.ps1"

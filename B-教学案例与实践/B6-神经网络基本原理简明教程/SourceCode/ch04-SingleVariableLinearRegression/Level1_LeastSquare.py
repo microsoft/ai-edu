76600c86e3eb426b5221395f976a15fff0ad228b
Xiaowu Hu <xiaowuhu@microsoft.com>
2019-06-08 17:02:07 +0800

    pp

--- B-教学案例与实践/B6-神经网络基本原理简明教程/SourceCode/ch04-SingleVariableLinearRegression/Level1_LeastSquare.py
+++ B-教学案例与实践/B6-神经网络基本原理简明教程/SourceCode/ch04-SingleVariableLinearRegression/Level1_LeastSquare.py
@@ -9,34 +9,38 @@ from pathlib import Path
 
 from HelperClass.SimpleDataReader import *
 
-# 根据公式8
+# 根据公式15
 def method1(X,Y,m):
-    x_mean = np.mean(X)
+    x_mean = X.mean()
     p = sum(Y*(X-x_mean))
     q = sum(X*X) - sum(X)*sum(X)/m
     w = p/q
     return w
 
-# 根据公式9
+# 根据公式16
 def method2(X,Y,m):
-    x_mean = sum(X)/m
-    y_mean = sum(Y)/m
+    x_mean = X.mean()
+    y_mean = Y.mean()
     p = sum(X*(Y-y_mean))
     q = sum(X*X) - x_mean*sum(X)
     w = p/q
     return w
 
-# 根据公式6
+# 根据公式13
 def method3(X,Y,m):
-    m = X.shape[0]
     p = m*sum(X*Y) - sum(X)*sum(Y)
     q = m*sum(X*X) - sum(X)*sum(X)
     w = p/q
     return w
 
-# 根据公式7
-def calculate_b(X,Y,w,m):
+# 根据公式14
+def calculate_b_1(X,Y,w,m):
     b = sum(Y-w*X)/m
+    return b
+
+# 根据公式9
+def calculate_b_2(X,Y,w):
+    b = Y.mean() - w * X.mean()
     return b
 
 if __name__ == '__main__':
@@ -46,13 +50,13 @@ if __name__ == '__main__':
     X,Y = reader.GetWholeTrainSamples()
     m = X.shape[0]
     w1 = method1(X,Y,m)
-    b1 = calculate_b(X,Y,w1,m)
+    b1 = calculate_b_1(X,Y,w1,m)
 
     w2 = method2(X,Y,m)
-    b2 = calculate_b(X,Y,w2,m)
+    b2 = calculate_b_2(X,Y,w2)
 
     w3 = method3(X,Y,m)
-    b3 = calculate_b(X,Y,w3,m)
+    b3 = calculate_b_1(X,Y,w3,m)
 
     print("w1=%f, b1=%f" % (w1,b1))
     print("w2=%f, b2=%f" % (w2,b2))

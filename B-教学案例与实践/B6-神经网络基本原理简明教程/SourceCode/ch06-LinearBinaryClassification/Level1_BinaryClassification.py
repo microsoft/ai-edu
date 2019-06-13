2457d33d5d473acf2c958a3edfdeb7c878a21430
Xiaowu Hu <xiaowuhu@microsoft.com>
2019-06-13 09:09:31 +0800

    update

--- B-教学案例与实践/B6-神经网络基本原理简明教程/SourceCode/ch06-LinearBinaryClassification/Level1_BinaryClassification.py
+++ B-教学案例与实践/B6-神经网络基本原理简明教程/SourceCode/ch06-LinearBinaryClassification/Level1_BinaryClassification.py
@@ -6,31 +6,7 @@ import numpy as np
 from HelperClass.NeuralNet import *
 from HelperClass.HyperParameters import *
 
-def ShowResult(net, dataReader):
-    fig = plt.figure(figsize=(6.5,6.5))
-    X,Y = dataReader.GetWholeTrainSamples()
-    for i in range(200):
-        if Y[i,0] == 1:
-            plt.scatter(X[i,0], X[i,1], marker='x', c='g')
-        else:
-            plt.scatter(X[i,0], X[i,1], marker='o', c='r')
-        #end if
-    #end for
-
-    x = np.array([0.58,0.92,0.62,0.55,0.39,0.29]).reshape(3,2)
-    a = net.inference(x)
-    print("A=", a)
-    for i in range(3):
-        if a[i,0] > 0.5:
-            plt.scatter(x[i,0], x[i,1], marker='^', c='g', s=100)
-        else:
-            plt.scatter(x[i,0], x[i,1], marker='^', c='r', s=100)
-        #end if
-    #end for
-    
-    plt.show()
-    
-
+   
 # 主程序
 if __name__ == '__main__':
     # data
@@ -44,7 +20,10 @@ if __name__ == '__main__':
     net.train(reader, checkpoint=1)
 
     # inference
-    ShowResult(net, reader)
+    x_predicate = np.array([0.58,0.92,0.62,0.55,0.39,0.29]).reshape(3,2)
+    a = net.inference(x_predicate)
+    print("A=", a)
+
 
 
 
--- B-教学案例与实践/B6-神经网络基本原理简明教程/SourceCode/ch06-LinearBinaryClassification/Level2_ShowBinaryResult.py
+++ B-教学案例与实践/B6-神经网络基本原理简明教程/SourceCode/ch06-LinearBinaryClassification/Level2_ShowBinaryResult.py
@@ -6,7 +6,17 @@ import numpy as np
 from HelperClass.NeuralNet import *
 from HelperClass.HyperParameters import *
 
-def ShowResult(net, dataReader):
+def draw_split_line(net,):
+    b12 = -net.B[0,0]/net.W[1,0]
+    w12 = -net.W[0,0]/net.W[1,0]
+    print(w12,b12)
+    x = np.linspace(0,1,10)
+    y = w12 * x + b12
+    plt.plot(x,y)
+    plt.axis([-0.1,1.1,-0.1,1.1])
+    plt.show()
+
+def draw_source_data(net, dataReader):
     fig = plt.figure(figsize=(6.5,6.5))
     X,Y = dataReader.GetWholeTrainSamples()
     for i in range(200):
@@ -17,6 +27,7 @@ def ShowResult(net, dataReader):
         #end if
     #end for
 
+def draw_predicate_data(net):
     x = np.array([0.58,0.92,0.62,0.55,0.39,0.29]).reshape(3,2)
     a = net.inference(x)
     print("A=", a)
@@ -27,9 +38,7 @@ def ShowResult(net, dataReader):
             plt.scatter(x[i,0], x[i,1], marker='^', c='r', s=100)
         #end if
     #end for
-    
-    plt.show()
-    
+ 
 
 # 主程序
 if __name__ == '__main__':
@@ -37,48 +46,22 @@ if __name__ == '__main__':
     reader = SimpleDataReader()
     reader.ReadData()
     # net
-    params = HyperParameters(eta=0.1, max_epoch=100, batch_size=10, eps=1e-3, net_type=NetType.BinaryClassifier)
+    params = HyperParameters(eta=0.1, max_epoch=100, batch_size=1, eps=1e-3, net_type=NetType.BinaryClassifier)
     input = 2
     output = 1
     net = NeuralNet(params, input, output)
     net.train(reader, checkpoint=1)
 
-    # inference
-    ShowResult(net, reader)
-
-
-
-
-def ShowData(X,Y):
-    for i in range(X.shape[1]):
-        if Y[0,i] == 0:
-            plt.plot(X[0,i], X[1,i], '.', c='r')
-        elif Y[0,i] == 1:
-            plt.plot(X[0,i], X[1,i], 'x', c='g')
-        # end if
-    # end for
+    # show result
+    draw_source_data(net, reader)
+    draw_predicate_data(net)
+    draw_split_line(net)
     plt.show()
 
-def ShowResult(X,Y,W,B,xt):
-    for i in range(X.shape[1]):
-        if Y[0,i] == 0:
-            plt.plot(X[0,i], X[1,i], '.', c='r')
-        elif Y[0,i] == 1:
-            plt.plot(X[0,i], X[1,i], 'x', c='g')
-        # end if
-    # end for
 
-    b12 = -B[0,0]/W[0,1]
-    w12 = -W[0,0]/W[0,1]
-    print(w12,b12)
-    x = np.linspace(0,1,10)
-    y = w12 * x + b12
-    plt.plot(x,y)
 
-    for i in range(xt.shape[1]):
-        plt.plot(xt[0,i], xt[1,i], '^', c='b')
 
-    plt.axis([-0.1,1.1,-0.1,1.1])
-    plt.show()
+
+
 
 

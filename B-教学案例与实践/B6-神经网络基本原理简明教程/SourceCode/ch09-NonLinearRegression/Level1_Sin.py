8a38ca5464160d876388523ab02456fd5531f9c8
Xiaowu Hu <xiaowuhu@microsoft.com>
2019-06-15 21:26:34 +0800

    pp

--- B-教学案例与实践/B6-神经网络基本原理简明教程/SourceCode/ch09-NonLinearRegression/Level1_Sin.py
+++ B-教学案例与实践/B6-神经网络基本原理简明教程/SourceCode/ch09-NonLinearRegression/Level1_Sin.py
@@ -12,10 +12,10 @@ file_name = "../../data/ch08.train.npz"
 
 class DataReaderEx(SimpleDataReader):
     def Add(self):
-        X = self.XTrain[:,0:1]**2
+        X = self.XTrain[:,]**2
         self.XTrain = np.hstack((self.XTrain, X))
-        X = self.XTrain[:,0:1]**3
-        self.XTrain = np.hstack((self.XTrain, X))
+        #X = self.XTrain[:,0:1]**3
+        #self.XTrain = np.hstack((self.XTrain, X))
 
 
 def ShowResult(net, dataReader, title):
@@ -25,7 +25,7 @@ def ShowResult(net, dataReader, title):
     # create and draw visualized validation data
     TX1 = np.linspace(0,1,100).reshape(100,1)
     TX = np.hstack((TX1, TX1[:,]**2))
-    TX = np.hstack((TX, TX1[:,]**3))
+    #TX = np.hstack((TX, TX1[:,]**3))
 
     TY = net.inference(TX)
     plt.plot(TX1, TY, 'x', c='r')
@@ -40,9 +40,9 @@ if __name__ == '__main__':
     print(dataReader.XTrain.shape)
 
     # net
-    params = HyperParameters(eta=0.1, max_epoch=10000, batch_size=10, eps=1e-3, net_type=NetType.Fitting)
-    num_input = 3
+    params = HyperParameters(eta=0.2, max_epoch=5000, batch_size=10, eps=0.03, net_type=NetType.Fitting)
+    num_input = 2
     num_output = 1
     net = NeuralNet(params, num_input, num_output)
-    net.train(dataReader, checkpoint=1)
-    ShowResult(net, dataReader, "Polynomial")
+    net.train(dataReader, checkpoint=10)
+    ShowResult(net, dataReader, params.toString())

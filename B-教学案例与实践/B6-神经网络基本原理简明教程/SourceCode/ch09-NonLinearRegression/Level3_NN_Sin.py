310464e80c2735859ddddd4557f7692d750a510c
Xiaowu Hu <xiaowuhu@microsoft.com>
2019-06-17 09:43:16 +0800

    op

--- B-教学案例与实践/B6-神经网络基本原理简明教程/SourceCode/ch09-NonLinearRegression/Level3_NN_Sin.py
+++ B-教学案例与实践/B6-神经网络基本原理简明教程/SourceCode/ch09-NonLinearRegression/Level3_NN_Sin.py
@@ -8,7 +8,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from HelperClass2.NeuralNet2 import *
 from HelperClass2.DataReader import *
 
-x_data_name = "../../Data/ch09.train.npz"
+x_data_name = "../../Data/ch08.train.npz"
 y_data_name = "../../Data/ch09.test.npz"
 
 def ShowResult(net, dataReader, title):
@@ -26,15 +26,15 @@ def ShowResult(net, dataReader, title):
 def ShowResult3D(net, title):
     # draw train data
     # create and draw visualized validation data
-    TX = np.linspace(0,1,100).reshape(100,1)
+    TX = np.linspace(0,1,10).reshape(10,1)
     TY = net.inference(TX)
     fig = plt.figure()
     ax = Axes3D(fig)
     #plt.plot(net.Z1[:,0],net.Z1[:,1],net.Z1[:,2],'.',c='black')
-    plt.plot(net.A1[:,0],net.A1[:,1],net.A1[:,2],'.',c='black')
+    print(net.A1)
+    print(net.Z2)
     plt.plot(net.A1[:,0],net.A1[:,1],net.Z2[:,0],'.',c='r')
-    plt.plot(net.A1[:,0],net.A1[:,2],net.Z2[:,0],'.',c='g')
-    plt.plot(net.A1[:,1],net.A1[:,2],net.Z2[:,0],'.',c='b')
+    plt.plot(net.A1[:,1],net.A1[:,0],net.Z2[:,0],'.',c='b')
     plt.show()
 
 #end def
@@ -55,7 +55,7 @@ def ShowResult2D(net, title):
     #plt.plot(TX,net.Z1[:,2],'.',c='b')
     plt.plot(TX,net.A1[:,0],'.',c='r')
     plt.plot(TX,net.A1[:,1],'.',c='g')
-    plt.plot(TX,net.A1[:,2],'.',c='b')
+    #plt.plot(TX,net.A1[:,2],'.',c='b')
     #plt.plot(net.Z2[:,0],net.A1[:,0],'.',c='r')
     #plt.plot(net.A1[:,1],net.Z2[:,0],'.',c='g')
     plt.show()
@@ -65,12 +65,12 @@ if __name__ == '__main__':
     dataReader.ReadData()
     dataReader.GenerateValidationSet()
 
-    n_input, n_hidden, n_output = 1, 3, 1
-    eta, batch_size, max_epoch = 0.1, 10, 10000
+    n_input, n_hidden, n_output = 1, 2, 1
+    eta, batch_size, max_epoch = 0.1, 10, 1000
     eps = 0.001
 
     hp = HyperParameters2(n_input, n_hidden, n_output, eta, max_epoch, batch_size, eps, NetType.Fitting, InitialMethod.Xavier)
-    net = NeuralNet2(hp, "model_131")
+    net = NeuralNet2(hp, "sin_121")
 
     net.LoadResult()
     print(net.wb1.W)
@@ -78,8 +78,12 @@ if __name__ == '__main__':
     print(net.wb2.W)
     print(net.wb2.B)
 
-    net.train(dataReader, 50, True)
+    #net.train(dataReader, 10, True)
     #net.ShowTrainingTrace()
     #ShowResult(net, dataReader, hp.toString())
-    #ShowResult3D(net, hp.toString())
-    ShowResult2D(net, hp.toString())
+    ShowResult3D(net, hp.toString())
+    #ShowResult2D(net, hp.toString())
+    #print(net.wb1.W)
+    #print(net.wb1.B)
+    #print(net.wb2.W)
+    #print(net.wb2.B)

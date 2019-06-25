81f0c9d064d68a06d464dedcbf6d86fe2ad92429
Xiaowu Hu <xiaowuhu@microsoft.com>
2019-06-25 12:44:09 +0800

    gg

--- B-教学案例与实践/B6-神经网络基本原理简明教程/SourceCode/ch11-NonLinearMultipleClassification/Level2_HiddenUnits.py
+++ B-教学案例与实践/B6-神经网络基本原理简明教程/SourceCode/ch11-NonLinearMultipleClassification/Level2_HiddenUnits.py
@@ -12,31 +12,35 @@ from HelperClass2.Visualizer import *
 train_data_name = "../../Data/ch11.train.npz"
 test_data_name = "../../Data/ch11.test.npz"
 
+def train(n_hidden):
+
+    n_input = dataReader.num_feature
+    n_output = dataReader.num_category
+    eta, batch_size, max_epoch = 0.1, 10, 10000
+    eps = 0.1
+
+    hp = HyperParameters2(n_input, n_hidden, n_output, eta, max_epoch, batch_size, eps, NetType.MultipleClassifier, InitialMethod.Xavier)
+    net = NeuralNet2(hp, "Bank_2N3")
+    net.train(dataReader, 100, True)
+    net.ShowTrainingTrace()
+    epoch = net.GetEpochNumber()
+
+    fig = plt.figure(figsize=(6,6))
+    ShowDataByOneHot2D(dataReader.XTrain[:,0], dataReader.XTrain[:,1], dataReader.YTrain, hp.toString())
+    ShowClassificationResult25D(net, 50, hp.toString() + ", epoch=" + str(epoch))
+    plt.show()
+
 if __name__ == '__main__':
     dataReader = DataReader(train_data_name, test_data_name)
     dataReader.ReadData()
     dataReader.NormalizeY(YNormalizationMethod.MultipleClassifier, base=1)
-
-    fig = plt.figure(figsize=(6,6))
-    ShowDataByOneHot2D(dataReader.XTrainRaw[:,0], dataReader.XTrainRaw[:,1], dataReader.YTrain, "Source Data")
-    plt.show()
-
     dataReader.NormalizeX()
     dataReader.Shuffle()
     dataReader.GenerateValidationSet()
 
-    n_input = dataReader.num_feature
-    n_hidden = 3
-    n_output = dataReader.num_category
-    eta, batch_size, max_epoch = 0.1, 10, 5000
-    eps = 0.1
-
-    hp = HyperParameters2(n_input, n_hidden, n_output, eta, max_epoch, batch_size, eps, NetType.MultipleClassifier, InitialMethod.Xavier)
-    net = NeuralNet2(hp, "Bank_233")
-    net.train(dataReader, 100, True)
-    net.ShowTrainingTrace()
-
-    fig = plt.figure(figsize=(6,6))
-    ShowDataByOneHot2D(dataReader.XTrain[:,0], dataReader.XTrain[:,1], dataReader.YTrain, hp.toString())
-    ShowClassificationResult25D(net, 50, hp.toString())
-    plt.show()
+#    train(2)
+#    train(4)
+#    train(8)
+#    train(16)
+#    train(32)
+    train(8)

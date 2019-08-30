7bea5fceec0567e1ef5c25bd1f1833872290b9b7
Xiaowu Hu <xiaowuhu@microsoft.com>
2019-08-30 16:35:01 +0800

    klo

--- B-教学案例与实践/B6-神经网络基本原理简明教程/SourceCode/ch18-CNNModel/Level1_ColorConvNet.py
+++ B-教学案例与实践/B6-神经网络基本原理简明教程/SourceCode/ch18-CNNModel/Level1_ColorConvNet.py
@@ -25,7 +25,7 @@ def LoadData(mode):
 
 def dnn_model():
     num_output = 6
-    max_epoch = 5
+    max_epoch = 20
     batch_size = 16
     learning_rate = 0.1
     params = HyperParameters_4_2(
@@ -56,9 +56,9 @@ def dnn_model():
 
 def cnn_model():
     num_output = 6
-    max_epoch = 50
+    max_epoch = 20
     batch_size = 16
-    learning_rate = 0.01
+    learning_rate = 0.1
     params = HyperParameters_4_2(
         learning_rate, max_epoch, batch_size,
         net_type=NetType.MultipleClassifier,
@@ -74,7 +74,7 @@ def cnn_model():
     p1 = PoolingLayer(c1.output_shape, (2,2), 2, PoolingTypes.MAX)
     net.add_layer(p1, "p1") 
 
-    c2 = ConvLayer(p1.output_shape, (8,3,3), (1,0), params)
+    c2 = ConvLayer(p1.output_shape, (3,3,3), (1,0), params)
     net.add_layer(c2, "c2")
     r2 = ActivationLayer(Relu())
     net.add_layer(r2, "relu2")
@@ -98,29 +98,38 @@ def cnn_model():
     return net
 
 def show_samples(x,y,title):
+    x = x/255
     fig,ax = plt.subplots(nrows=8, ncols=8, figsize=(10,10))
     for i in range(64):
         ax[i//8,i%8].imshow(x[i].transpose(1,2,0))
-        ax[i//8,i%8].set_title(name[np.argmax(y[i])])
+        ax[i//8,i%8].set_title(name[y[i,0]])
         ax[i//8,i%8].axis('off')
     #endfor
     plt.suptitle(title)
     plt.show()
 
 
-if __name__ == '__main__':
-    # for cnn
+def train_cnn():
     dataReader = LoadData('image')
     net = cnn_model()
-    x,y = dataReader.GetBatchTrainSamples(64, 0)
-    # enf for cnn
+    return net, dataReader
 
-    show_samples(x,y,"sample")
-    
+def train_dnn():
+    dataReader = LoadData('vector')
+    net = dnn_model()
+    return net, dataReader
+
+if __name__ == '__main__':
+    net, dataReader = train_cnn()
+    #net, dataReader = train_dnn()
+    x,y = dataReader.XTrainRaw[0:64], dataReader.YTrainRaw[0:64]
+    show_samples(x,y,"color:sample")
     net.train(dataReader, checkpoint=0.5, need_test=True)
     net.ShowLossHistory(XCoordinate.Iteration)
-  
+    
     X_test,Y_test = dataReader.GetTestSet()
-    X = X_test[0:64].reshape(64,3,28,28)
-    Z = net.inference(X)
-    show_samples(X,Z,"predication")
+    Z = net.inference(X_test[0:64])
+    X = dataReader.XTestRaw[0:64]
+    #show_samples(X,np.argmax(Z,axis=1).reshape(64,1),"dnn:predication")
+    show_samples(X,np.argmax(Z,axis=1).reshape(64,1),"cnn:predication")
+    

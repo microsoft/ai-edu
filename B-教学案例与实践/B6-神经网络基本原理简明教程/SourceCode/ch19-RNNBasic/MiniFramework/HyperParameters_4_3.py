dfd78d8eacd56388ab09698da2b16c314983020d
xiaowuhu <hu.xiaowu@hotmail.com>
2019-10-08 09:47:27 +0800

    Xiaowuhu/190927 (#349)
    * fty
    
    * Update 19.2-更多时序的RNN.md
    
    * Update Level3_RnnCell.py
    
    * jjh
    
    * Update 16.0-正则化.md
    
    * jkhkj
    
    * dwer
    
    * add level3 for rnn

--- B-教学案例与实践/B6-神经网络基本原理简明教程/SourceCode/ch19-RNNBasic/MiniFramework/HyperParameters_4_3.py
+++ B-教学案例与实践/B6-神经网络基本原理简明教程/SourceCode/ch19-RNNBasic/MiniFramework/HyperParameters_4_3.py
@@ -3,14 +3,13 @@
 
 from MiniFramework.EnumDef_6_0 import *
 
-# this class is for two-layer NN only
-class HyperParameters_4_2(object):
-    def __init__(self, eta=0.1, max_epoch=10000, batch_size=5,
+# this class is for RNN only
+class HyperParameters_4_3(object):
+    def __init__(self, eta, max_epoch, batch_size,
+                 num_step, num_input, num_hidden, num_output,
                  net_type=NetType.Fitting, 
                  init_method=InitialMethod.Xavier,
-                 optimizer_name=OptimizerName.SGD,
-                 stopper = None,
-                 regular_name=RegularMethod.Nothing, regular_value=1.0):
+                 optimizer_name=OptimizerName.SGD):
         self.eta = eta
         self.max_epoch = max_epoch
         # if batch_size == -1, it is FullBatch
@@ -22,12 +21,11 @@ class HyperParameters_4_2(object):
         self.net_type = net_type
         self.init_method = init_method
         self.optimizer_name = optimizer_name
-        self.stopper = stopper
-        self.regular_name = regular_name
-        self.regular_value = regular_value
+        self.num_step = num_step
+        self.num_input = num_input
+        self.num_hidden = num_hidden
+        self.num_output = num_output
 
     def toString(self):
         title = str.format("bz:{0},eta:{1},init:{2},op:{3}", self.batch_size, self.eta, self.init_method.name, self.optimizer_name.name)
-        if self.regular_name != RegularMethod.Nothing:
-            title += str.format(",rgl:{0}:{1}", self.regular_name.name, self.regular_value)
         return title

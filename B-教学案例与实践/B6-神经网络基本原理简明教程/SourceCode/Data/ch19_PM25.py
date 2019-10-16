68cbf39d15305e9ed89356d54a204691c9815ee0
xiaowuhu <hu.xiaowu@hotmail.com>
2019-10-16 09:37:10 +0800

    ght

---
+++ B-教学案例与实践/B6-神经网络基本原理简明教程/SourceCode/Data/ch19_PM25.py
@@ -0,0 +1,41 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+from pandas import read_csv
+from datetime import datetime
+from matplotlib import pyplot
+
+# load data
+def parse(x):
+    return datetime.strptime(x, '%Y %m %d %H')
+
+"""
+dataset = read_csv('../../data/PM25_data.csv',  parse_dates = [['year', 'month', 'day', 'hour']], index_col=0, date_parser=parse)
+dataset.drop('No', axis=1, inplace=True)
+# manually specify column names
+dataset.columns = ['pollution', 'dew', 'temp', 'press', 'wnd_dir', 'wnd_spd', 'snow', 'rain']
+dataset.index.name = 'date'
+# mark all NA values with 0
+dataset['pollution'].fillna(0, inplace=True)
+# drop the first 24 hours
+dataset = dataset[24:]
+# summarize first 5 rows
+print(dataset.head(5))
+# save to file
+dataset.to_csv('pollution.csv')
+"""
+
+# load dataset
+dataset = read_csv('pollution.csv', header=0, index_col=0)
+values = dataset.values
+# specify columns to plot
+groups = [0, 1, 2, 3, 5, 6, 7]
+i = 1
+# plot each column
+pyplot.figure()
+for group in groups:
+    pyplot.subplot(len(groups), 1, i)
+    pyplot.plot(values[:, group])
+    pyplot.title(dataset.columns[group], y=0.5, loc='right')
+    i += 1
+pyplot.show()

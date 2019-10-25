cb21289c24cea969fc7cd64fd88ecdb4c2746036
Zixuan Chen <v-zich@microsoft.com>
2019-10-25 14:20:30 +0800

    初次案例提交
    加入TTS案例

---
+++ B-教学案例与实践/B15-定制化 - 语音服务应用案例 - TTS应用/微软-方案1/TTS_Demo/TTS_Demo/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace TTS_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.transferButton = new System.Windows.Forms.Button();
+            this.playButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.tips = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 22);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(592, 230);
+            this.textBox1.TabIndex = 0;
+
+            // 
+            // transferButton
+            // 
+            this.transferButton.Location = new System.Drawing.Point(12, 283);
+            this.transferButton.Name = "transferButton";
+            this.transferButton.Size = new System.Drawing.Size(110, 48);
+            this.transferButton.TabIndex = 1;
+            this.transferButton.Text = "转换";
+            this.transferButton.UseVisualStyleBackColor = true;
+            this.transferButton.Click += new System.EventHandler(this.transferButton_Click);
+            // 
+            // playButton
+            // 
+            this.playButton.Enabled = false;
+            this.playButton.Location = new System.Drawing.Point(425, 296);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(75, 23);
+            this.playButton.TabIndex = 2;
+            this.playButton.Text = "播放";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Enabled = false;
+            this.saveButton.Location = new System.Drawing.Point(529, 296);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 3;
+            this.saveButton.Text = "保存";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // tips
+            // 
+            this.tips.AutoSize = true;
+            this.tips.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tips.Location = new System.Drawing.Point(205, 296);
+            this.tips.Name = "tips";
+            this.tips.Size = new System.Drawing.Size(0, 24);
+            this.tips.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 362);
+            this.Controls.Add(this.tips);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.transferButton);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "文本转语音";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button transferButton;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label tips;
+    }
+}
+
---
+++ B-教学案例与实践/B15-定制化 - 语音服务应用案例 - TTS应用/微软-方案1/TTS_Demo/TTS_Demo/Form1.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.IO;
+using System.Media;
+using System.Windows.Forms.VisualStyles;
+
+namespace TTS_Demo
+{
+    
+
+    public partial class Form1 : Form
+    {
+        
+        string tempFile = "temp.wav"; //临时文件存储路径
+
+        string accessToken;
+        Authentication auth = new Authentication("https://westus.api.cognitive.microsoft.com/sts/v1.0/issuetoken","5f7e7c7254ef4415bd14950350ebc0c5");
+        string host = "https://westus.tts.speech.microsoft.com/cognitiveservices/v1";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        //点击“转换”按钮
+        private async void transferButton_Click(object sender, EventArgs e)
+        {
+
+            playButton.Enabled = false;
+            saveButton.Enabled = false ;
+            tips.Text = "语音生成中...";
+
+            string text = textBox1.Text;
+
+            await textToSpeechAsync(text,tempFile);
+
+            tips.Text = "";
+            playButton.Enabled = true;
+            saveButton.Enabled = true;
+
+        }
+
+        //点击“播放”按钮
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer playSound = new SoundPlayer(tempFile);
+            playSound.Play();
+        }
+
+        //点击“保存”按钮
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            saveSpeech();
+        }
+
+
+        //转换文本并保存
+        private async Task textToSpeechAsync(string text,string savePath)
+        {
+            try
+            {
+                accessToken = await auth.FetchTokenAsync().ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            string body = @"<speak version='1.0' xmlns='https://www.w3.org/2001/10/synthesis' xml:lang='en-US'>
+              <voice name='Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)'>" +
+              text + "</voice></speak>";
+
+            using (var client = new HttpClient())
+            {
+                using (var request = new HttpRequestMessage())
+                {
+                    // Set the HTTP method
+                    request.Method = HttpMethod.Post;
+                    // Construct the URI
+                    request.RequestUri = new Uri(host);
+                    // Set the content type header
+                    request.Content = new StringContent(body, Encoding.UTF8, "application/ssml+xml");
+                    // Set additional header, such as Authorization and User-Agent
+                    request.Headers.Add("Authorization", "Bearer " + accessToken);
+                    request.Headers.Add("Connection", "Keep-Alive");
+                    // Update your resource name
+                    request.Headers.Add("User-Agent", "YOUR_RESOURCE_NAME");
+                    request.Headers.Add("X-Microsoft-OutputFormat", "riff-24khz-16bit-mono-pcm");
+                    // Create a request
+                    Console.WriteLine("Calling the TTS service. Please wait... \n");
+                    using (var response = await client.SendAsync(request).ConfigureAwait(false))
+                    {
+                        response.EnsureSuccessStatusCode();
+                        // Asynchronously read the response
+                        using (var dataStream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false))
+                        {
+                            Console.WriteLine("savePath:"+savePath);
+                            using (var fileStream = new FileStream(savePath, FileMode.Create, FileAccess.Write, FileShare.Write))
+                            {
+                                await dataStream.CopyToAsync(fileStream).ConfigureAwait(false);
+                                fileStream.Close();
+                            }
+     
+                        }
+                    }
+                }
+            }
+
+        }
+
+        //存储生成的音频
+        private void saveSpeech()
+        {
+            string filePath = "";
+
+            string fileName = textBox1.Text.Substring(0,10);
+
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.FileName = fileName;
+            saveFile.Filter = "音频文件 (*.wav) | *.wav";
+            //保存对话框是否记忆上次打开的目录 
+            saveFile.RestoreDirectory = true;
+            
+            //点了保存按钮进入 
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                filePath = saveFile.FileName.ToString(); //获得文件路径
+
+                if (File.Exists(tempFile))
+                {
+                    File.Copy(tempFile, filePath , true);
+                }
+            }
+  
+        }
+
+
+    }
+}

133809a386d017507ea2d77e084539981088d460
FariyAhill <zuoanhuating@163.com>
2020-08-26 14:55:41 +0800

    Update DropoutLayer.py
    fix bug

--- A-基础教程/A2-神经网络基本原理简明教程/SourceCode/ch17-CNNBasic/MiniFramework/DropoutLayer.py
+++ A-基础教程/A2-神经网络基本原理简明教程/SourceCode/ch17-CNNBasic/MiniFramework/DropoutLayer.py
@@ -1,6 +1,8 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+import numpy as np
+
 from MiniFramework.EnumDef_6_0 import *
 from MiniFramework.Layer import *
 

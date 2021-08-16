8e1a95ca921e7d157d283181ff9f8b530196be09
xiaowuhu <hu.xiaowu@hotmail.com>
2021-08-16 08:25:40 +0800

    update

---
+++ 基础教程/A4-经典机器学习算法（更新中）/第1步 回归算法/src/2-2-NormalEquationSample.py
@@ -0,0 +1,49 @@
+import numpy as np
+import sys
+import os
+from pathlib import Path
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+def load_file(file_name):
+    file_path = generate_file_path(file_name)
+    file = Path(file_path)
+    if file.exists():
+        samples = np.loadtxt(file, delimiter=',')
+        return samples
+    else:
+        return None
+
+def generate_file_path(file_name):
+    curr_path = sys.argv[0]
+    curr_dir = os.path.dirname(curr_path)
+    file_path = os.path.join(curr_dir, file_name)
+    return file_path
+
+def normal_equation(X,Y):
+    num_example = X.shape[0]
+    # 在原始的X矩阵最左侧加一列1
+    ones = np.ones((num_example,1))
+    x = np.column_stack((ones, X))    
+    # X^T * X
+    p = np.dot(x.T, x)
+    # (X^T * X)^{-1}
+    I = np.eye(p.shape[0]) * 1e-6
+    p = p + I
+    q = np.linalg.inv(p)
+    # (X^T * X)^{-1} * X^T
+    r = np.dot(q, x.T)
+    # (X^T * X)^{-1} * X^T * Y
+    A = np.dot(r, Y)
+    # 按顺序
+    b = A[0]
+    a1 = A[1]
+    a2 = A[2]
+    return a1, a2, b
+
+if __name__ == '__main__':
+    X = np.array([[1, 2, 3, 4],[1, 3, 4, 3]]).T
+    Y = np.array([3.2, 4.1, 4.8, 5.1]).T
+    #Y = np.array([3, 4, 5, 5]).T
+    a1, a2, b = normal_equation(X,Y)
+    print(str.format("a1={0:.4f}, a2={1:.4f}, b={2:.4f}", a1, a2, b))

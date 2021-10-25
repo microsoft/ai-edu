b1d48d3858ca91c073e0b23822d83e368325d5a9
huxiaowu <hu.xiaowu@hotmail.com>
2021-10-25 19:45:21 +0800

    update

--- 基础教程/A4-经典机器学习算法（更新中）/第5步 非线性分类算法/第11章 支持向量机扩展/src/Code_11_9_1_Gaussian.py
+++ 基础教程/A4-经典机器学习算法（更新中）/第5步 非线性分类算法/第11章 支持向量机扩展/src/Code_11_9_1_Gaussian.py
@@ -5,7 +5,7 @@ import matplotlib.cm as cm
 import matplotlib as mpl
 from scipy import integrate
 
-def gaussian_2d(x, sigma, mu):
+def gaussian_1d(x, sigma, mu):
     a = - (x-mu)**2 / (2*sigma*sigma)
     f = np.exp(a) / (sigma * np.sqrt(2*np.pi))
     return f
@@ -20,6 +20,19 @@ def f(x):
 def integrate_fab():
     return integrate.quad(f,-100,100)
 
+# 高斯函数图像
+# sample - 样本点
+# gamma - 控制形状
+# weight - 控制权重（正负及整体升高或降低）
+# scope - 网格数据范围及密度
+def gaussian_2d_fun(sample, gamma, scope):
+    # 生成网格数据
+    xx = np.linspace(scope[0], scope[1], scope[2])
+    yy = np.linspace(scope[3], scope[4], scope[5])
+    P,Q = np.meshgrid(xx, yy)
+    # 二维高斯函数 * 权重 -> 三维高斯曲面
+    R = np.exp(-gamma * ((P-sample[0])**2 + (Q-sample[1])**2))
+    return P,Q,R
 
 if __name__ == '__main__':
 
@@ -40,7 +53,7 @@ if __name__ == '__main__':
     mu = [0,0,2,0]
     linestyles=['-', '-.', ':', '--']
     for i in range(4):
-        f = gaussian_2d(x, sigma[i], mu[i])
+        f = gaussian_1d(x, sigma[i], mu[i])
         label = str.format("μ={0},σ={1}", mu[i], sigma[i])
         ax1.plot(x, f, linestyle=linestyles[i], label=label)
     ax1.legend()
@@ -49,8 +62,8 @@ if __name__ == '__main__':
     # 2. 两个高斯函数的乘积，表示相关性
     sigma = 1
     x = np.linspace(-5,5,50)
-    fa = gaussian_2d(x, sigma, 1)
-    fb = gaussian_2d(x, sigma, -2)
+    fa = gaussian_1d(x, sigma, 1)
+    fb = gaussian_1d(x, sigma, -2)
     fab = fa * fb
     
     ax2 = fig.add_subplot(122)
@@ -63,3 +76,19 @@ if __name__ == '__main__':
 
     plt.show()
 
+
+    fig = plt.figure()
+    ax1 = plt
+    #ax1.set_title(u'样本的二维高斯曲面')
+    scope = [-0.5,1.5,50, -0.5,1.5,50]
+    #set_ax(ax1, scope)
+    gamma = 1
+    P,Q,R1 = gaussian_2d_fun([0,1], gamma, scope)
+    ax1.contour(P, Q, R1, c='red') #cmap=cm.coolwarm)
+    P,Q,R2 = gaussian_2d_fun([1,0], gamma, scope)
+    ax1.contour(P, Q, R2, c='blue') #cmap=cm.coolwarm)
+    R = R1 * R2
+    ax1.contour(P, Q, R, c='green') # cmap=cm.coolwarm)
+
+
+    plt.show()

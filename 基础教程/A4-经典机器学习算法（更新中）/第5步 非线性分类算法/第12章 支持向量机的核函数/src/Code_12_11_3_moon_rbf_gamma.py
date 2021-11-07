d0abf2b4aa93552aa12845e48465f738f082e03f
xiaowuhu <hu.xiaowu@hotmail.com>
2021-11-07 21:03:23 +0800

    update

--- 基础教程/A4-经典机器学习算法（更新中）/第5步 非线性分类算法/第12章 支持向量机的核函数/src/Code_12_11_3_moon_rbf_gamma.py
+++ 基础教程/A4-经典机器学习算法（更新中）/第5步 非线性分类算法/第12章 支持向量机的核函数/src/Code_12_11_3_moon_rbf_gamma.py
@@ -11,6 +11,7 @@ import sys
 import os
 from pathlib import Path
 import matplotlib as mpl
+import matplotlib.cm as cm
 from matplotlib.colors import ListedColormap
 
 def generate_file_path(file_name):
@@ -27,47 +28,18 @@ def load_data(file_name, n_samples):
         X = samples[:, 0:2]
         Y = samples[:, 2]
     else:
-        X, Y = make_moons(n_samples=n_samples, noise=1, shuffle=False)
+        X, Y = make_moons(n_samples=n_samples, noise=0.3, shuffle=False)
         Y[Y == 0] = -1
         samples = np.hstack((X,Y.reshape(-1,1)))
         np.savetxt(file_path, samples, fmt='%f, %f, %d', delimiter=',', header='x1, x2, y')
     return X, Y
 
-# 高斯函数图像
-# sample - 样本点
-# gamma - 控制形状
-# weight - 控制权重（正负及整体升高或降低）
-# scope - 网格数据范围及密度
-def gaussian_2d(sample, gamma, weight, scope):
-    # 生成网格数据
-    xx = np.linspace(scope[0], scope[1], scope[2])
-    yy = np.linspace(scope[3], scope[4], scope[5])
-    P,Q = np.meshgrid(xx, yy)
-    # 二维高斯函数 * 权重 -> 三维高斯曲面
-    R = weight * np.exp(-gamma * ((P-sample[0])**2 + (Q-sample[1])**2))
-    return P,Q,R
-
-
-# 显示各个样本独立的二维高斯函数平面投影
-def show_sample_gaussian(ax, gamma, X, Y, scope):
-    set_ax(ax, scope)
-    ax.set_title(u"以各个样本为中心的高斯函数投影")
-
-    for idx in range(X.shape[0]):
-        P,Q,R = gaussian_2d(X[idx], gamma, Y[idx], scope)
-        if (Y[idx]==1):
-            c = 'red'
-        else:
-            c = 'blue'
-        ax.contour(P,Q,R,2,colors=c,linewidths=[0.5,0.2],linestyles='dashed')
-
-    draw_2d_samples(ax, X, Y)
-
 
 # 绘图区基本设置
 def set_ax(ax, scope):
-    ax.axis('equal')
-    ax.grid()
+    if (ax.name != '3d'):
+        ax.grid()
+        ax.axis('equal')
     ax.set_xlabel("x1")
     ax.set_ylabel("x2")
     if (scope is not None):
@@ -108,6 +80,33 @@ def show_predication_result(ax, model, X, Y, scope):
     else:
         draw_2d_samples(ax, X, Y, display_text=False)
 
+
+# 显示等高线分类结果
+def show_contour_result(ax, model, X, Y, scope):   
+    set_ax(ax, scope)
+
+    # 生成测试数据，形成一个点阵来模拟平面
+    x1 = np.linspace(scope[0], scope[1], scope[2])
+    x2 = np.linspace(scope[3], scope[4], scope[5])
+    X1,X2 = np.meshgrid(x1,x2)
+    # 从行列变形为序列数据
+    X12 = np.c_[X1.ravel(), X2.ravel()]
+    # 做预测
+    pred = model.decision_function(X12)
+    # 从序列数据变形行列形式
+    y_pred = pred.reshape(X1.shape)
+
+    # 绘图
+    P,Q = np.meshgrid(x1, x2)
+    ax.contour(P, Q, y_pred, cmap=cm.coolwarm)
+    #ax.plot_surface(P, Q, y_pred, cmap=cm.coolwarm)
+    # 绘制原始样本点用于比对
+    if (X.shape[0]<=10):
+        draw_2d_samples(ax, X, Y, display_text=True)
+    else:
+        draw_2d_samples(ax, X, Y, display_text=False)
+
+
 # 高斯核函数 SVM
 def rbf_svc(X, Y, C, gamma):
     model = SVC(C=C, gamma = gamma, kernel='rbf')
@@ -115,61 +114,67 @@ def rbf_svc(X, Y, C, gamma):
 
     #print("权重:",np.dot(model.dual_coef_, model.support_vectors_))
     print("支持向量个数:",model.n_support_)
-    #print("支持向量索引:",model.support_)
+    print("支持向量索引:",model.support_)
     #print("支持向量:",np.round(model.support_vectors_,3))
-    #print("支持向量ay:",np.round(model.dual_coef_,3))
+    print("支持向量ay:",np.round(model.dual_coef_,3))
+    print("偏移值:",model.intercept_)
     print("准确率:", model.score(X, Y))
 
     return model
 
-
-
-def compare_3_gamma(X, Y):
+def compare_3_gamma(X, Y, gamma):
 
     scope = [-3,3,100,-3,3,100]
-
-    fig = plt.figure()
     plt.axis('off')
+    C = 1
 
-    gamma = 1
-    C = 2
-    model = rbf_svc(X, Y, C, gamma)
-    ax1 = fig.add_subplot(131)
-    show_predication_result(ax1, model, X, Y, scope)
-    ax1.set_title(u"gamma=1")
+    for i in range(3):
+        gamma_str = str.format("gamma={0}", gamma[i])
+        print(gamma_str)
+        model = rbf_svc(X, Y, C, gamma[i])
+        ax = plt.subplot(1,3,i+1)
+        ax.set_title(gamma_str)
+        show_predication_result(ax, model, X, Y, scope)
 
-    gamma = 10
-    C = 2
-    model = rbf_svc(X, Y, C, gamma)
-    ax2 = fig.add_subplot(132)
-    show_predication_result(ax2, model, X, Y, scope)
-    ax2.set_title(u"gamma=10")
+    plt.show()
 
-
-    gamma = 100
-    C = 2
-    model = rbf_svc(X, Y, C, gamma)
-    ax3 = fig.add_subplot(133)
-    show_predication_result(ax3, model, X, Y, scope)
-    ax3.set_title(u"gamma=100")
+def show_3_contour(X, Y, gamma):
+    scope = [-3,3,100,-3,3,100]
+    plt.axis('off')
+    C = 1
+    
+    for i in range(3):
+        gamma_str = str.format("gamma={0}", gamma[i])
+        print(gamma_str)
+        model = rbf_svc(X, Y, C, gamma[i])
+        #ax = plt.subplot(1,3,i+1, projection='3d')
+        ax = plt.subplot(1,3,i+1)
+        ax.set_title(gamma_str)
+        show_contour_result(ax, model, X, Y, scope)
 
     plt.show()
 
 
 if __name__=="__main__":
-
+    # 10 个样本
     file_name = "Data_11_11_moon_10.csv"
     X_raw, Y = load_data(file_name, 10)
-
+    # 标准化
     ss = StandardScaler()
     X = ss.fit_transform(X_raw)
+    # 比较分类区域
+    gamma = [1,5,10]
+    compare_3_gamma(X, Y, gamma)
+    # 比较高斯核函数作用范围
+    show_3_contour(X, Y, gamma)
 
-    compare_3_gamma(X, Y)
-
+    # 100 个样本
     file_name = "Data_11_11_moon_100.csv"
     X_raw, Y = load_data(file_name, 100)
-
+    # 标准化
     ss = StandardScaler()
     X = ss.fit_transform(X_raw)
-
-    compare_3_gamma(X, Y)
+    # 比较分类区域
+    gamma = [1,10,100]
+    compare_3_gamma(X, Y, gamma)
+    #show_3_contour(X, Y, gamma)

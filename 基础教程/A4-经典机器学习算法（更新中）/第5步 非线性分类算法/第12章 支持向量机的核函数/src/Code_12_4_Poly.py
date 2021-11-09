682d3e581305f6c3dafca603ef8c00f52e5bac12
xiaowuhu <hu.xiaowu@hotmail.com>
2021-11-09 09:46:55 +0800

    update

---
+++ 基础教程/A4-经典机器学习算法（更新中）/第5步 非线性分类算法/第12章 支持向量机的核函数/src/Code_12_4_Poly.py
@@ -0,0 +1,142 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import *
+from sklearn.svm import SVC
+from sklearn.preprocessing import *
+import sys
+import os
+from pathlib import Path
+import matplotlib as mpl
+from matplotlib.colors import ListedColormap
+
+
+def generate_file_path(file_name):
+    curr_path = sys.argv[0]
+    curr_dir = os.path.dirname(curr_path)
+    file_path = os.path.join(curr_dir, file_name)
+    return file_path
+
+def load_data(file_name):
+    file_path = generate_file_path(file_name)
+    file = Path(file_path)
+    if file.exists():
+        samples = np.loadtxt(file, delimiter=',')
+        X = samples[:, 0:2]
+        Y = samples[:, 2]
+    else:
+        X, Y = make_circles(n_samples=100, factor=0.5, noise=0.1)
+        Y[Y == 0] = -1
+        samples = np.hstack((X,Y.reshape(-1,1)))
+        np.savetxt(file_path, samples, fmt='%f, %f, %d', delimiter=',', header='x1, x2, y')
+    return X, Y
+
+
+# 绘制平面样本点
+def draw_2d_samples(ax, x, y, display_text=True):
+    ax.scatter(x[y==1,0], x[y==1,1], marker='^', color='red')
+    ax.scatter(x[y==-1,0], x[y==-1,1], marker='o', color='blue')
+    if (display_text):
+        for i in range(x.shape[0]):
+            ax.text(x[i,0], x[i,1]+0.1, str(i))
+
+
+# 多项式核函数 SVM
+def poly_svc(X, Y, C, degree):
+    model = SVC(C=C, kernel='poly', degree=degree)
+    model.fit(X,Y)
+
+    print("权重:",np.dot(model.dual_coef_, model.support_vectors_))
+    print("支持向量个数:",model.n_support_)
+    #print("支持向量索引:",model.support_)
+    #print("支持向量:",np.round(model.support_vectors_,3))
+    #print("支持向量ay:",np.round(model.dual_coef_,3))
+    print("准确率:", model.score(X, Y))
+
+    return model
+
+# 绘图区基本设置
+def set_ax(ax, scope):
+    ax.axis('equal')
+    ax.grid()
+    ax.set_xlabel("x1")
+    ax.set_ylabel("x2")
+    if (scope is not None):
+        ax.set_xlim(scope[0], scope[1])
+        ax.set_ylim(scope[3], scope[4])
+
+# 显示分类区域结果
+def show_predication_result(ax, model, X, Y, scope):   
+    # 生成测试数据，形成一个点阵来模拟平面
+    x1 = np.linspace(scope[0], scope[1], scope[2])
+    x2 = np.linspace(scope[3], scope[4], scope[5])
+    X1,X2 = np.meshgrid(x1,x2)
+    # 从行列变形为序列数据
+    X12 = np.c_[X1.ravel(), X2.ravel()]
+    # 做预测
+    #pred = model.predict(X12)
+    pred = model.decision_function(X12)
+    # 从序列数据变形行列形式
+    y_pred = pred.reshape(X1.shape)
+
+    # 绘图
+    cmap = ListedColormap(['yellow','lightgray'])
+    #plt.contourf(X1,X2, y_pred, cmap=cmap)
+    plt.contourf(X1,X2, y_pred)
+    # 绘制原始样本点用于比对
+    if (X.shape[0]<=10):
+        draw_2d_samples(ax, X, Y, display_text=True)
+    else:
+        draw_2d_samples(ax, X, Y, display_text=False)
+
+def classification(X_raw, Y):
+    ss = StandardScaler()
+    X = ss.fit_transform(X_raw)
+
+    fig = plt.figure()
+    mpl.rcParams['font.sans-serif'] = ['SimHei']  
+    mpl.rcParams['axes.unicode_minus']=False
+
+    scope = [-2.5,2.5,100,-2.5,2.5,100]    
+
+    C = 1
+    degree = 2
+    model = poly_svc(X, Y, C, degree)
+    # 原始样本
+    ax1 = fig.add_subplot(131)
+    set_ax(ax1, scope)
+    ax1.set_title(u"degree=2")
+    show_predication_result(ax1, model, X, Y, scope)
+
+    degree = 3
+    model = poly_svc(X, Y, C, degree)
+    ax2 = fig.add_subplot(132)
+    set_ax(ax2, scope)
+    ax2.set_title(u"degree=3")
+    show_predication_result(ax2, model, X, Y, scope)
+
+    degree = 4
+    model = poly_svc(X, Y, C, degree)
+    ax3 = fig.add_subplot(133)
+    set_ax(ax3, scope)
+    ax3.set_title(u"degree=4")
+    show_predication_result(ax3, model, X, Y, scope)
+
+  
+
+
+    plt.show()
+
+
+if __name__=="__main__":
+
+    X_raw = np.array([[-1.5,0], [-1,0], [-0.5,0], [0,0], [0.5,0], [1,0], [1.5,0]])
+    Y = np.array([-1,-1,1,1,1,-1,-1])
+    classification(X_raw, Y)
+
+    X_raw, Y = load_data("Data_12_circle_100.csv")
+    classification(X_raw, Y)
+
+    X_raw, Y = load_data("Data_12_moon_100.csv")
+    classification(X_raw, Y)
+
+ 
--- 基础教程/A4-经典机器学习算法（更新中）/第5步 非线性分类算法/第12章 支持向量机的核函数/src/Code_12_8_Moon_Rbf.py
+++ 基础教程/A4-经典机器学习算法（更新中）/第5步 非线性分类算法/第12章 支持向量机的核函数/src/Code_12_8_Moon_Rbf.py
@@ -1,8 +1,6 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy import linalg
-from numpy.linalg.linalg import norm
 from sklearn.datasets import *
 from sklearn.svm import *
 from sklearn.pipeline import *

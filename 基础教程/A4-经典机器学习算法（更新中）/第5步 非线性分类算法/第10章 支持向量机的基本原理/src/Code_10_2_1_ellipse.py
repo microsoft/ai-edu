6271aa20cbd0868dfe4ed3c0d494c9147dc5761b
xiaowuhu <hu.xiaowu@hotmail.com>
2021-11-14 19:39:34 +0800

    update

--- 基础教程/A4-经典机器学习算法（更新中）/第5步 非线性分类算法/第10章 支持向量机的基本原理/src/Code_10_2_1_ellipse.py
+++ 基础教程/A4-经典机器学习算法（更新中）/第5步 非线性分类算法/第10章 支持向量机的基本原理/src/Code_10_2_1_ellipse.py
@@ -6,12 +6,13 @@ def draw_left_3d(fig):
     ax = fig.add_subplot(121, projection='3d')
     ax.set_title(u"约束平面与曲面相交")
 
-    # 绘制 x^2+y^2 曲面
+    # 绘制 x^2+2y^2 曲面
     x = np.linspace(-3, 3, 100)
     y = np.linspace(-3, 3, 100)
     P, Q = np.meshgrid(x, y)
     R = P * P + Q * Q * 2
     ax.plot_surface(P, Q, R, alpha=0.5)
+    ax.contour(P, Q, R, [0.5,1.5,2.67,3.5,4.5], zdir='z', offset=0)
     ax.set_xlabel("x")
     ax.set_ylabel("y")
     
@@ -21,7 +22,7 @@ def draw_left_3d(fig):
     X, Y = np.meshgrid(x, y)
     P2 = X
     Q2 = -X-2
-    R2 = 20*np.maximum(Y,0) # 增加平面的高度
+    R2 = 15*np.maximum(Y,0) # 增加平面的高度
     ax.plot_surface(P2, Q2, R2, color='y', alpha=0.6)
 
     #两平面交线
@@ -41,12 +42,13 @@ def draw_left_3d(fig):
 def draw_right_2d(fig, P, Q, R):
     ax2 = fig.add_subplot(122)
     ax2.grid()
+    ax2.axis('equal')
     ax2.set_xlabel("x")
     ax2.set_ylabel("y")
     ax2.set_title(u"约束直线与投影等高线相切")
 
     # 绘制等高线
-    c = ax2.contour(P, Q, R, levels=np.linspace(-1, 5, 50))
+    c = ax2.contour(P, Q, R, [0.5,1.5,2.67,3.5,4.5])
     ax2.clabel(c,inline=1,fontsize=10)
     # 绘制约束直线
     x = np.linspace(-3, 1, 100)
@@ -54,8 +56,21 @@ def draw_right_2d(fig, P, Q, R):
     ax2.plot(x, y)
     ax2.scatter(-4/3, -2/3, c='r')
 
+def f(x):
+    return x**4+6*x**3+17*x**2-12*x-16
+
+def aaa():
+    X = np.linspace(1.0841, 1.0842, 100)
+    print(X)
+    for x in X:
+        print(x, f(x))
+
 
 if __name__ == '__main__':
+
+    aaa()
+    exit()
+
     mpl.rcParams['font.sans-serif'] = ['SimHei']  
     mpl.rcParams['axes.unicode_minus']=False
     fig = plt.figure()

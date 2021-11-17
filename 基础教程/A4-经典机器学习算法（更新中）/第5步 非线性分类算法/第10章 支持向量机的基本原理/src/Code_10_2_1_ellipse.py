02e2799423fb7d98bd61779d435efd7f60ad4157
xiaowuhu <hu.xiaowu@hotmail.com>
2021-11-17 09:41:05 +0800

    update

--- 基础教程/A4-经典机器学习算法（更新中）/第5步 非线性分类算法/第10章 支持向量机的基本原理/src/Code_10_2_1_ellipse.py
+++ 基础教程/A4-经典机器学习算法（更新中）/第5步 非线性分类算法/第10章 支持向量机的基本原理/src/Code_10_2_1_ellipse.py
@@ -30,12 +30,14 @@ def draw_left_3d(ax):
             if (abs(hh(xx,yy)) <= 0.05):
                 ax.scatter(xx,yy,gg(xx,yy),color='r',s=5)
 
+    #return P,Q,R
+
     # 绘制约束圆
     #return P,Q,R
     # 生成圆柱数据，底面半径为r，高度为h。
     # 先根据极坐标方式生成数据
     u = np.linspace(0,2*np.pi,50)  # 把圆分按角度为50等分
-    h = np.linspace(0,20,20)        # 把高度1均分为20份
+    h = np.linspace(0,20,2)        # 把高度1均分为20份
     x = np.outer(np.sin(u),np.ones(len(h)))-2  # x值重复20次
     y = np.outer(np.cos(u),np.ones(len(h)))-1  # y值重复20次
     z = np.outer(np.ones(len(u)),h)   # x，y 对应的高度

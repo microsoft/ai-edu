6236c99b9a4c093f2827016889b052c49839237e
xiaowuhu <hu.xiaowu@hotmail.com>
2022-02-12 19:08:55 +0800

    Update MPR_Student_Value.py

--- 基础教程/A7-强化学习/03-马尔可夫决策过程/src/MPR_Student_Value.py
+++ 基础教程/A7-强化学习/03-马尔可夫决策过程/src/MPR_Student_Value.py
@@ -7,18 +7,21 @@ def Student_MonteCarol(gamma):
     episodes = 10000
     end_states = [ds.States.Sleep]
     v = algoM.MonteCarol(ds.Rewards, ds.Matrix, ds.States, end_states, gamma, episodes)
+    print("----Monte Carol----")
     for start_state in ds.States:
         print(start_state, "= {:.2f}".format(v[start_state.value]))
 
 
 def InvMatrix(gamma):
     v = algoM.Matrix(ds, gamma)
+    print("----Matrix----")
     for start_state in ds.States:
         print(start_state, "= {:.2f}".format(v[start_state.value]))
     return v
 
 def Bellman(gamma):
     v = algoM.Bellman(ds.States, ds.Matrix, ds.Rewards, gamma)
+    print("----Bellman----")
     for start_state in ds.States:
         print(start_state, "= {:.2f}".format(v[start_state.value]))
 

69d4402c22067a118cdcb49a05bb81b52a917fca
xiaowuhu <hu.xiaowu@hotmail.com>
2022-03-02 10:01:13 +0800

    ffg

--- 基础教程/A7-强化学习/04-无模型的预测/src/Algorithm_MC_RMSE.py
+++ 基础教程/A7-强化学习/04-无模型的预测/src/Algorithm_MC_RMSE.py
@@ -3,82 +3,39 @@ import multiprocessing as mp
 import math
 import numpy as np
 
-'''
-# 多状态同时更新的蒙特卡洛采样
-def MC_wrong(V, ds, start_state, episodes, alpha, gamma):
-    for i in range(episodes):
-        trajectory = []
-        curr_state = start_state
-        trajectory.append((curr_state.value, 0))
-        while True:
-            # 到达终点，结束一幕，退出循环开始算分
-            if (ds.is_end_state(curr_state)):
-                break
-            # 左右随机游走
-            next_state, reward = ds.step(curr_state)
-            #endif
-            trajectory.append((next_state.value, reward))
-            curr_state = next_state
-            #endif
-        #endwhile
-        # calculate G,V
-        G = 0
-        # 从后向前遍历，因为 G = R_t+1 + gamma * R_t + gamme^2 * R_t-1 + ...
-        for j in range(len(trajectory)-1, -1, -1):
-            state_value, reward = trajectory[j]
-            G = gamma * G + reward
+def RMSE(a, b):
+    err = np.sqrt(np.sum(np.square(a - b))/b.shape[0])
+    return err
 
-        # 更新从状态开始到终止状态之前的所有V值
-        for (state_value, reward) in trajectory[0:-1]:
-            # math: V(s) \leftarrow V(s) + \alpha (G_t - V(s))
-            # 这个实现有bug，G并不是G_t
-            V[state_value] = V[state_value] + alpha * (G - V[state_value])
-        
-    #endfor
-    return V
-'''
-
-'''
-def MonteCarol(ds, gamma, episodes):
-    pool = mp.Pool(processes=4)
-    Vs = []
-    results = []
-    for start_state in ds.get_states():
-        results.append(
-            pool.apply_async(MC1, args=(ds, start_state, episodes, gamma,)))
-
-    pool.close()
-    pool.join()
-    for i in range(len(results)):
-        v = results[i].get()
-        Vs.append(v)
-
-    return Vs
-'''
+def calculate_error(errors, episode, every_n_episode, V, ground_truth):
+    if (episode % every_n_episode == 0):
+        err = RMSE(V, ground_truth)
+        errors.append(err)
 
 # 一边生成序列一边计算
 # 每一幕的中间状态都抛弃不用（造成浪费），只计算起始状态的V值
 # 首次访问型(只对start_state计算V值)
-def MC1(ds, episodes, gamma):
-    V = np.zeros(ds.num_states)
-    # 遍历每个状态(做为起始状态)
-    for i in tqdm.trange(ds.num_states):
-        start_state = ds.get_states()(i)
-        # 终止状态，直接返回
-        if (ds.is_end_state(start_state)):
-            # 最后一个状态也可能有reward值
-            V[start_state.value] = ds.get_reward(start_state)
-            continue
+def MC1(ds, episodes, gamma, ground_truth, every_n_episode):
+    V = np.zeros((ds.num_states))
+    errors = []
+    # 多幕采样
+    for episode in tqdm.trange(episodes):
 
-        # 对多个幕的结果求均值=期望E[G]
-        sum_g = 0
-        # 多幕采样
-        for episode in range(episodes):
+        # 遍历每个状态(做为起始状态)
+        for i in range(ds.num_states):
+
+            start_state = ds.get_states()(i)
+            # 终止状态，直接返回
+            if (ds.is_end_state(start_state)):
+                # 最后一个状态也可能有reward值
+                V[start_state.value] += ds.get_reward(start_state)
+                continue
+
             curr_state = start_state
             # math: g = r1 + \gamma*r2 + \gamma^2*r3 + ...
             g = 0
             power = 0
-            # 对每一幕
+
             while (True):
                 if ds.is_end_state(curr_state):
                     break
@@ -88,15 +45,19 @@ def MC1(ds, episodes, gamma):
                 power += 1
                 curr_state = next_state
             # end while
-            sum_g += g
+            V[start_state.value] += g
         # end for
-        V[start_state.value] = sum_g / episodes
-    return V
+
+        calculate_error(errors, episode+1, every_n_episode, V/(episode+1), ground_truth)
+
+    return errors
 
 # MC1的改进，利用每个状态位
-def MC2(ds, start_state, episodes, gamma):
+def MC2(ds, start_state, episodes, gamma, ground_truth, every_n_episode):
     Vs = np.zeros((ds.num_states,2))  # state[total value, count of g]
-    for i in tqdm.trange(episodes):
+    Vs[:,1] = 1 # 避免被除数为0
+    errors = []
+    for episode in tqdm.trange(episodes):
         trajectory = []
         curr_state = start_state
         trajectory.append((curr_state.value, 0))
@@ -109,7 +70,7 @@ def MC2(ds, start_state, episodes, gamma):
             #endif
             trajectory.append((next_state.value, reward))
             curr_state = next_state
-
+        #endwhile
         num_step = len(trajectory)
         g = 0
         # 从后向前遍历，因为 G = R_t+1 + gamma * R_t + gamme^2 * R_t-1 + ...
@@ -118,10 +79,11 @@ def MC2(ds, start_state, episodes, gamma):
             g = gamma * g + reward
             Vs[state_value, 0] += g     # total value
             Vs[state_value, 1] += 1     # count
+        #endfor
 
-    V = Vs[:,0] / Vs[:,1]
+        calculate_error(errors, episode+1, every_n_episode, Vs[:,0] / Vs[:,1], ground_truth)
     #endfor
-    return V
+    return errors
 
 
 # 多状态同时更新的蒙特卡洛采样

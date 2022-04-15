58fde1669bfd5ee757a4ee408c9557993d296a70
xiaowuhu <xiaowuhu@microsoft.com>
2022-04-15 17:11:42 +0800

    fix

--- 基础教程/A7-强化学习/draft/70-学生学习问题 - 贝尔曼方程/src/dp_1.py
+++ 基础教程/A7-强化学习/draft/70-学生学习问题 - 贝尔曼方程/src/dp_1.py
@@ -3,17 +3,17 @@ import time
 # 递归法
 def f1(n):
     if n==1 or n==2:
-        return 1
+        return n
     else:
-        fn = f1(n-1) + f1(n-2)
+        fn = f1(n-1) + f1(n-2)  # 递归调用函数本身两次
         return fn
 
 # 迭代法
 def f2(n):
-    fn_2 , fn_1 = 1, 1
+    fn_2 , fn_1 = 1, 2
     for i in range(n-2):
-        fn = fn_2 + fn_1
-        fn_2 = fn_1
+        fn = fn_2 + fn_1    # 计算
+        fn_2 = fn_1         # 迭代更换上一次的数值便于下次计算
         fn_1 = fn
     return fn
 
@@ -27,15 +27,18 @@ def f3(n):
 
 
 if __name__=="__main__":
+    n = 41
+
+
     start = time.time()
-    print("递归法结果 =", f1(35))
+    print("递归法结果 =", f1(n))
     end1 = time.time()
     print("递归法耗时 =", end1-start)
     
-    print("迭代法结果 = ", f2(35))
+    print("迭代法结果 = ", f2(n))
     end2 = time.time()
     print("迭代法耗时 =", end2-end1)
 
-    print("备忘录法结果 =", f3(10))
+    print("备忘录法结果 =", f3(n))
     end3 = time.time()
     print("备忘录法耗时 =", end3-end2)

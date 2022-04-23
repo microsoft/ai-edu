20853b48cc5cd9133af17390359dc1bc5ae414be
xiaowuhu <hu.xiaowu@hotmail.com>
2022-04-23 11:43:34 +0800

    update

--- 基础教程/A7-强化学习/draft/20-租车问题 - 转移概率矩阵/src/People_Move.py
+++ 基础教程/A7-强化学习/draft/20-租车问题 - 转移概率矩阵/src/People_Move.py
@@ -2,27 +2,29 @@
 import numpy as np
 
 P = np.array([
-    [0.6, 0.4],
+    [0.7, 0.3],
     [0.1, 0.9]
 ])
 
-def Check_Convergence(P):
-    P_curr = P.copy()
-    for i in range(100000):
-        P_next=np.dot(P,P_curr)
-        print("迭代次数 =",i+1)
+def Check_Convergence(P, People_orginal):
+    P_next = P.copy()
+    People_next = People_orginal.copy()
+    for i in range(1000000):
+        People_curr = People_next.copy()
+        print("---- 迭代次数 =",i+1)
+        People_next = np.round(np.dot(People_orginal, P_next), 0)
+        print("当前人口数量（农村,城市）", People_next)
+        P_next = np.dot(P, P_next)
         print(P_next)
-        if np.allclose(P_curr, P_next):
+        if np.allclose(People_next, People_curr):
             break
-        P_curr = P_next
     return P_next
 
 if __name__=="__main__":
-    Pn = Check_Convergence(P)
-    
     people = np.array([100000,20000])
     print("最初的农村-城市人口数量为：")
     print(people)
-    final=np.dot(people, Pn)
-    print("最终的农村-城市人口数量为：")
-    print(np.around(final, 0))
+    Pn = Check_Convergence(P, people)
+    print("最终的农村-城市人口数量为：", np.around(np.dot(people, Pn),0))
+    print("收敛的转移概率：")
+    print(np.round(Pn,2))

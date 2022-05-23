345c73cbd762e7cebafbb5a35cc2622e35f836e4
xiaowuhu <xiaowuhu@microsoft.com>
2022-05-23 20:46:17 +0800

    update

--- 基础教程/A7-强化学习/70-开发流程问题 - 从价值估算到价值计算/src/LifeCycle_2_Matrix_Iteration.py
+++ 基础教程/A7-强化学习/70-开发流程问题 - 从价值估算到价值计算/src/LifeCycle_2_Matrix_Iteration.py
@@ -1,27 +1,24 @@
 import numpy as np
-import CodeLifeCycle_0_DataModel_P as dm
+import LifeCycle_0_DataModel as data
+import common_helper as helper
 
 # 矩阵迭代法
 def matrix_iteration(dataModel, gamma):
-    print("---矩阵迭代法---")
-    V_next = np.zeros(dataModel.N)
+    print("矩阵迭代法")
+    helper.print_seperator_line(helper.SeperatorLines.long)
+    V_new = np.zeros(dataModel.N)
     count = 0   # 迭代计数器
     while (count < 1000):   # 1000 是随意指定的一个比较大的数，避免不收敛而导致while无限
         count += 1  # 计数器+1
-        V = V_next.copy()   # 准备一个备份，用于比较，检查是否收敛
-        V_next = dataModel.R + gamma * np.dot(dataModel.P, V)   # 式 3
-        if np.allclose(V_next, V):  # 检查收敛性
+        V_old = V_new.copy()   # 准备一个备份，用于比较，检查是否收敛
+        V_new = dataModel.R + gamma * np.dot(dataModel.P, V_old)   # 式 3
+        if np.allclose(V_new, V_old):  # 检查收敛性
             break
     print("迭代次数 :", count)
-    return V
-
-def print_V(V):
-    print(V)
-    vv = np.around(V,3)
-    for s in dataModel.S:
-        print(str.format("{0}:\t{1}", s.name, vv[s.value]))
+    return V_new
 
 def check_convergence(dataModel):
+    helper.print_seperator_line(helper.SeperatorLines.long)
     print("迭代100次, 检查状态转移矩阵是否趋近于 0: ")
     P_new = dataModel.P.copy()
     for i in range(100):
@@ -29,10 +26,10 @@ def check_convergence(dataModel):
     print(np.around(P_new, 3))
 
 if __name__=="__main__":
-    dataModel = dm.DataModel()
+    dataModel = data.DataModel()
     gamma = 1
 
     V = matrix_iteration(dataModel, gamma)
-    print_V(V)
+    helper.print_V(dataModel, V)
 
     check_convergence(dataModel)
--- 基础教程/A7-强化学习/70-开发流程问题 - 从价值估算到价值计算/src/LifeCycle_4_Bellman_Equation_Iteration.py
+++ 基础教程/A7-强化学习/70-开发流程问题 - 从价值估算到价值计算/src/LifeCycle_4_Bellman_Equation_Iteration.py
@@ -1,5 +1,6 @@
 import numpy as np
 from enum import Enum
+import common_helper as helper
 
 # 状态
 class States(Enum):
@@ -11,20 +12,18 @@ class States(Enum):
     Merge = 5
     End = 6
 
-# 奖励向量，顺序与States定义一致
-#Rewards = [-3, -2, -1, 0, 5, -4, 0]
 # 奖励向量 缺陷 编码 测试 审查 重构 合并 结束
 Rewards = [-3, 0,   +1, +3,  +2, -1,  0]
 
 # 用字典代替状态转移矩阵
 D = {
-    States.Bug: [(States.Bug, 0.7),(States.Coding, 0.3)],
-    States.Coding: [(States.Bug, 0.6),(States.Test, 0.4)],
-    States.Test: [(States.Review, 0.9),(States.End, 0.1)],
-    States.Review: [(States.Refactor, 0.2),(States.Merge, 0.8)],
-    States.Refactor: [(States.Coding, 0.2),(States.Test, 0.5),(States.Review, 0.3)],
-    States.Merge: [(States.End, 1.0)],
-    States.End: [(States.End, 1.0)]
+    States.Bug:     [(States.Bug, 0.2),     (States.Coding, 0.8)],
+    States.Coding:  [(States.Bug, 0.6),     (States.Test, 0.4)],
+    States.Test:    [(States.Coding, 0.1),  (States.Review, 0.9)],
+    States.Review:  [(States.Coding, 0.1),  (States.Refactor, 0.2), (States.Merge, 0.7)],
+    States.Refactor:[(States.Coding, 0.2),  (States.Test, 0.5),     (States.Review, 0.3)],
+    States.Merge:   [(States.End, 1.0)],
+    States.End:     [(States.End, 1.0)]
 }
 
 class DataModel(object):
@@ -41,38 +40,33 @@ class DataModel(object):
 
 
 # 贝尔曼方程单数组就地更新
-def Bellman_iteration_single_array(dataModel, gamma):
-    print("---单数组就地更新法---")
+def Bellman_iteration(dataModel, gamma, max_iteration):
+    print("单数组就地更新法")
+    helper.print_seperator_line(helper.SeperatorLines.long)
     V = np.zeros(dataModel.N)
     count = 0
-    while (count < 1000):   # 1000 是随意指定的一个比较大的数，避免不收敛而导致while无限
+    while (count < max_iteration): 
         count += 1
         V_old = V.copy()
         # 遍历每一个 state 作为 curr_state
-        for curr_state in dataModel.S:
+        for s in dataModel.S:
             # 得到转移概率
-            list_state_prob  = dataModel.get_next(curr_state)
+            list_state_prob  = dataModel.get_next(s)
             # 计算 \sum(P·V)
-            v_sum = 0
-            for next_state, next_prob in list_state_prob:
-                v_sum += next_prob * V[next_state.value]
+            v_s_next = 0
+            for s_next, p_s_s_next in list_state_prob:
+                v_s_next += p_s_s_next * V[s_next.value]
             # 计算 V = R + gamma * \sum(P·V)
-            V[curr_state.value] = dataModel.R[curr_state.value] + gamma * v_sum
+            V[s.value] = dataModel.R[s.value] + gamma * v_s_next
         # 检查收敛性
         if np.allclose(V, V_old):
             break
     print("迭代次数 :", count)
     return V
 
-def print_V(V):
-    print(V)
-    vv = np.around(V,3)
-    for s in dataModel.S:
-        print(str.format("{0}:\t{1}", s.name, vv[s.value]))
-
 if __name__=="__main__":
     dataModel = DataModel()
     gamma = 1
-
-    V = Bellman_iteration_single_array(dataModel, gamma)
-    print_V(V)
+    max_iteration = 1000
+    V = Bellman_iteration(dataModel, gamma, max_iteration)
+    helper.print_V(dataModel, V)

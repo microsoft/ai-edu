e58d5ed55d34c9fc108088801577ad6a2d3ab641
xiaowuhu <xiaowuhu@microsoft.com>
2022-06-20 10:12:51 +0800

    up

--- 基础教程/A7-强化学习/20-多臂强盗问题 - 从概率计算到模拟验证/src/bandit_25_softmax.py
+++ 基础教程/A7-强化学习/20-多臂强盗问题 - 从概率计算到模拟验证/src/bandit_25_softmax.py
@@ -4,41 +4,33 @@ import multiprocessing as mp
 import bandit_20_base as kab_base
 
 class KAB_Softmax(kab_base.KArmBandit):
-    def __init__(self, k_arms=10, alpha:float=0.1, temperature=1):
+    def __init__(self, k_arms=10, alpha:float=0.1):
         super().__init__(k_arms=k_arms)
         self.alpha = alpha
         self.action_prob = 0
-        self.tmpt = temperature
-
 
     def reset(self):
         super().reset()
         self.average_reward = 0
 
-
     def select_action(self):
-        q_exp = np.exp((self.Q - np.max(self.Q))/self.tmpt)
-        self.action_prob = q_exp / np.sum(q_exp)
-        action = np.random.choice(self.k_arms, p=self.action_prob)
+        q_exp = np.exp(self.Q - np.max(self.Q))     # 所有的值都减去最大值
+        self.action_prob = q_exp / np.sum(q_exp)    # softmax 实现
+        action = np.random.choice(self.k_arms, p=self.action_prob)  # 按概率选择动作
         return action
-  
-    
-    def update_Q(self, action, reward):
-        # 总次数(time)
-        self.steps += 1
-        # 动作次数(action_count)
-        self.action_count[action] += 1
 
-        # 计算动作价值，偏好函数
+    def update_Q(self, action, reward):
+        self.steps += 1 # 迭代次数
+        self.action_count[action] += 1  # 动作次数(action_count)
+        # 计算动作价值
         one_hot = np.zeros(self.k_arms)
         one_hot[action] = 1
         self.average_reward += (reward - self.average_reward) / self.steps
         self.Q += self.alpha * (reward - self.average_reward) * (one_hot - self.action_prob)
-    
-        
-    def simulate(self, runs, steps):
-        rewards, best_action, actions = super().simulate(runs, steps)
-        return rewards, best_action, actions
+
+    # def simulate(self, runs, steps):
+    #     rewards, best_action, actions = super().simulate(runs, steps)
+    #     return rewards, best_action, actions
 
 if __name__ == "__main__":
     runs = 2000
@@ -46,49 +38,17 @@ if __name__ == "__main__":
     k_arms = 10 
     
     bandits:kab_base.KArmBandit = []
-    bandits.append(KAB_Softmax(k_arms, alpha=0.1, temperature=1))
-    bandits.append(KAB_Softmax(k_arms, alpha=0.2, temperature=1))
-    bandits.append(KAB_Softmax(k_arms, alpha=0.3, temperature=1))
-    bandits.append(KAB_Softmax(k_arms, alpha=0.4, temperature=1))
+    bandits.append(KAB_Softmax(k_arms, alpha=0.10))
+    bandits.append(KAB_Softmax(k_arms, alpha=0.15))
+    bandits.append(KAB_Softmax(k_arms, alpha=0.20))
+    bandits.append(KAB_Softmax(k_arms, alpha=0.25))
 
     labels = [
-        'Softmax(0.1,1), ',
-        'Softmax(0.2,1), ',
-        'Softmax(0.3,1), ',
-        'Softmax(0.4,1), ',
+        'Softmax(0.10)',
+        'Softmax(0.15)',
+        'Softmax(0.20)',
+        'Softmax(0.25)',
     ]
 
-    title = r'Softmax ($\alpha$=0.1,0.2,0.3,0.4)'
+    title = 'Softmax'
     kab_base.mp_simulate(bandits, k_arms, runs, steps, labels, title)
-
-    
-    bandits:kab_base.KArmBandit = []
-    bandits.append(KAB_Softmax(k_arms, alpha=0.15, temperature=1))
-    bandits.append(KAB_Softmax(k_arms, alpha=0.2, temperature=1))
-    bandits.append(KAB_Softmax(k_arms, alpha=0.25, temperature=1))
-    bandits.append(KAB_Softmax(k_arms, alpha=0.3, temperature=1))
-
-    labels = [
-        'Softmax(0.15,1), ',
-        'Softmax(0.2,1), ',
-        'Softmax(0.25,1), ',
-        'Softmax(0.3,1), ',
-    ]
-    title = r'Softmax ($\alpha$=0.15,0.2,0.25,0.3)'
-    kab_base.mp_simulate(bandits, k_arms, runs, steps, labels, title)
-    
-    bandits:kab_base.KArmBandit = []
-    bandits.append(KAB_Softmax(k_arms, alpha=0.15, temperature=0.5))
-    bandits.append(KAB_Softmax(k_arms, alpha=0.15, temperature=0.75))
-    bandits.append(KAB_Softmax(k_arms, alpha=0.15, temperature=1))
-    bandits.append(KAB_Softmax(k_arms, alpha=0.15, temperature=2))
-
-    labels = [
-        'Softmax(0.15,0.6), ',
-        'Softmax(0.15,0.8), ',
-        'Softmax(0.15,1), ',
-        'Softmax(0.15,2), ',
-    ]
-    title = r'Softmax ($\tau$=0.6,0.8,1,2)'
-    kab_base.mp_simulate(bandits, k_arms, runs, steps, labels, title)
-    

c313973401aa48f22d34034515fa239e2bb658da
xiaowuhu <xiaowuhu@microsoft.com>
2022-06-22 10:19:20 +0800

    update

--- 基础教程/A7-强化学习/20-多臂强盗问题 - 从概率计算到模拟验证/src/bandit_25_softmax_test.py
+++ 基础教程/A7-强化学习/20-多臂强盗问题 - 从概率计算到模拟验证/src/bandit_25_softmax_test.py
@@ -27,31 +27,32 @@ class KAB_Softmax_test(KAB_Softmax):
 if __name__ == "__main__":
     runs = 1
     steps = 200
-    k_arms = 10 
+    k_arms = 3
     bandit = KAB_Softmax_test(k_arms, alpha=0.15)
     bandit.simulate(runs, steps)
 
-    
+    grid = plt.GridSpec(nrows=1, ncols=2)
+    plt.subplot(grid[0, 0])
     Q_array = np.array(bandit.Qs)
     for i in range(k_arms):
         plt.plot(Q_array[:,i], label=str(i))
-    plt.title(u'10个动作的价值变化')
+    plt.title(u'备选动作的价值变化')
     plt.xlabel(u'迭代次数')
     plt.ylabel(u'动作价值')
     plt.grid()
     plt.legend()
-    plt.show()
-
-
     print("最终的动作价值 =", np.round(Q_array[-1,:], 2))
-    print("动作价值的和 =", np.sum(Q_array[-1,:]))
 
-    prob_array = np.array(bandit.Ps)
+    plt.subplot(grid[0, 1])
+    P_array = np.array(bandit.Ps)
     for i in range(k_arms):
-        plt.plot(prob_array[:,i], label=str(i))
-    plt.title(u'10个动作备选概率的变化')
+        plt.plot(P_array[:,i], label=str(i))
+    plt.title(u'备选动作概率的变化')
     plt.xlabel(u'迭代次数')
     plt.ylabel(u'被选概率')
     plt.grid()
     plt.legend()
     plt.show()
+    
+    print("最终的备选概率 =", P_array[-1,:])
+    print("备选概率的和 =", np.sum(P_array[-1,:]))

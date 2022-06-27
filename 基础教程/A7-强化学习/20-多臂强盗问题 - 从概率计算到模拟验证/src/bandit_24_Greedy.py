96ed38c5eecfcd16d1d88af4c73482da3b0e07fb
xiaowuhu <xiaowuhu@microsoft.com>
2022-06-27 18:22:52 +0800

    Xiaowuhu/20220606 (#740)
    * update
    
    * Create car.py
    
    * add files
    
    * update
    
    * Update 1-赌博机研究平台.md
    
    * Update 0-第2章.md
    
    * Update 1-赌博机研究平台.md
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * up
    
    * Update 6-置信上界算法.md
    
    * up
    
    * up
    
    * Update 6-置信上界算法.md
    
    * update
    
    * Create 6-置信上界算法 copy.md
    
    * Update 6-置信上界算法 copy.md
    
    * Update 6-置信上界算法.md
    
    * up
    
    * update
    
    * up
    
    * update
    
    * jhjkhjk
    
    * hhl
    
    * klhkj
    
    * update
    
    * update
    
    * update

---
+++ 基础教程/A7-强化学习/20-多臂强盗问题 - 从概率计算到模拟验证/src/bandit_24_Greedy.py
@@ -0,0 +1,35 @@
+import numpy as np
+import bandit_23_Base as kab_base
+
+class KAB_Greedy(kab_base.KArmBandit):
+    def __init__(self, k_arms=10, try_steps=10):
+        super().__init__(k_arms=k_arms)
+        self.try_steps = try_steps  # 试探次数
+
+    def select_action(self):
+        if (self.steps < self.try_steps):
+            action = np.random.randint(self.k_arms) # 随机选择动作
+        else:
+            action = np.argmax(self.Q)  # 贪心选择目前最好的动作
+        return action
+
+
+if __name__ == "__main__":
+    runs = 2000
+    steps = 1000
+    k_arms = 10
+
+    bandits:kab_base.KArmBandit = []
+    bandits.append(KAB_Greedy(k_arms, try_steps=10))
+    bandits.append(KAB_Greedy(k_arms, try_steps=20))
+    bandits.append(KAB_Greedy(k_arms, try_steps=40))
+    bandits.append(KAB_Greedy(k_arms, try_steps=80))
+
+    labels = [
+        'Greedy(10)',
+        'Greedy(20)',
+        'Greedy(40)',
+        'Greedy(80)'
+    ]
+    title = "Greedy"
+    kab_base.mp_simulate(bandits, k_arms, runs, steps, labels, title)

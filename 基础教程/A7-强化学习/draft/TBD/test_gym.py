086ec244872c3be279ef808482fcea9c59b451ee
xiaowuhu <xiaowuhu@microsoft.com>
2022-06-28 11:22:41 +0800

    update

---
+++ 基础教程/A7-强化学习/draft/TBD/test_gym.py
@@ -0,0 +1,17 @@
+import gym
+#env = gym.make("LunarLander-v2")
+env = gym.make("CartPole-v1")
+env.action_space.seed(42)
+
+observation, info = env.reset(seed=42, return_info=True)
+
+for _ in range(1000):
+    observation, reward, done, info = env.step(env.action_space.sample())
+    env.render()
+
+    if done:
+        observation, info = env.reset(return_info=True)
+        print("done")
+        print(observation)
+
+env.close()

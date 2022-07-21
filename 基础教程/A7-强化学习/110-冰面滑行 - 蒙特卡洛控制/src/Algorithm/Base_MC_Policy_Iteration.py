f385060a36bc24fc7aacf3818d8fbaa235f6e7b7
xiaowuhu <xiaowuhu@microsoft.com>
2022-07-21 09:55:21 +0800

    update

--- 基础教程/A7-强化学习/110-冰面滑行 - 蒙特卡洛控制/src/Algorithm/Base_MC_Policy_Iteration.py
+++ 基础教程/A7-强化学习/110-冰面滑行 - 蒙特卡洛控制/src/Algorithm/Base_MC_Policy_Iteration.py
@@ -3,23 +3,24 @@ import tqdm
 
 # 策略迭代
 class Policy_Iteration(object):
-    def __init__(self, env, policy, gamma, episodes, final, check_policy_method=0):
-        self.policy = policy
-        self.episodes = episodes
-        self.final = final
-        self.env = env
-        self.gamma = gamma
-        self.check_policy_method = check_policy_method
-        self.nA = self.env.action_space.n
-        self.nS = self.env.observation_space.n
-        self.n_iteration = 0
-        self.Value = np.zeros((self.nS, self.nA))  # G 的总和
-        self.Count = np.zeros((self.nS, self.nA))  # G 的数量
+    # 初始化
+    def __init__(self, env, policy, gamma, rough, final, check_policy_method=0):
+        self.policy = policy                        # 初始策略
+        self.rough_episodes = rough                 # 粗预测的分幕循环次数
+        self.final_episodes = final                 # 细预测的分幕循环次数
+        self.env = env                              # 环境
+        self.gamma = gamma                          # 折扣
+        self.check_method = check_policy_method     # 迭代终止条件
+        self.nA = self.env.action_space.n           # 动作空间
+        self.nS = self.env.observation_space.n      # 状态空间
+        self.n_iteration = 0                        # 分幕循环次数总和
+        self.Value = np.zeros((self.nS, self.nA))   # G 的总和
+        self.Count = np.zeros((self.nS, self.nA))   # G 的数量
+
 
     # 策略评估
     def policy_evaluation(self, episodes):
         for _ in tqdm.trange(episodes):   # 多幕循环
-        #for _ in range(episodes):   # 多幕循环
             self.n_iteration += 1
             # 重置环境，开始新的一幕采样
             s = self.env.reset()
@@ -30,16 +31,14 @@ class Policy_Iteration(object):
                 next_s, reward, done, _ = self.env.step(action)
                 Episode.append((s, action, reward))
                 s = next_s
-
-            num_step = len(Episode)
-            G = 0
             # 从后向前遍历计算 G 值
-            for t in range(num_step-1, -1, -1):
+            G = 0
+            for t in range(len(Episode)-1, -1, -1):
                 s, a, r = Episode[t]
                 G = self.gamma * G + r
                 self.Value[s,a] += G     # 值累加
                 self.Count[s,a] += 1     # 数量加 1
-
+        # 多幕循环结束，计算 Q 值
         self.Count[self.Count==0] = 1 # 把分母为0的填成1，主要是针对终止状态Count为0
         Q = self.Value / self.Count   # 求均值
         return Q
@@ -52,7 +51,7 @@ class Policy_Iteration(object):
     def policy_iteration(self):
         while True:
             print("策略评估")
-            Q = self.policy_evaluation(self.episodes)
+            Q = self.policy_evaluation(self.rough_episodes)
             print("策略改进")
             old_policy = self.policy.copy()
             self.policy_improvement(Q)
@@ -60,7 +59,7 @@ class Policy_Iteration(object):
                 print("新旧策略相等")
                 break
         print("精准的策略评估")
-        Q = self.policy_evaluation(self.final)
+        Q = self.policy_evaluation(self.final_episodes)
         return Q, self.policy
 
     def check_policy_diff(self, old_policy, new_policy):

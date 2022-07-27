ecec76ea367ecaf4ed9f168fba28b6571126d59c
xiaowuhu <xiaowuhu@microsoft.com>
2022-07-27 10:23:13 +0800

    update

--- 基础教程/A7-强化学习/110-二十一点问题 - 蒙特卡洛控制/src/MC_111_ES.py
+++ 基础教程/A7-强化学习/110-二十一点问题 - 蒙特卡洛控制/src/MC_111_ES.py
@@ -1,45 +1,62 @@
-from email import policy
 import numpy as np
 import gym
-import Algorithm.Algo_OptimalValueFunction as algoDP
 import Algorithm.Base_MC_Policy_Iteration as base
-import common.DrawQpi as drawQ
-import common.CommonHelper as helper
 import matplotlib.pyplot as plt
-import matplotlib as mpl
+import seaborn as sns
+import tqdm
 
-mpl.rcParams['font.sans-serif'] = ['SimHei']  
-mpl.rcParams['axes.unicode_minus'] = False
+HIT = 1
+STICK = 0
 
-
-class MC_Greedy(base.Policy_Iteration):
-    def policy_improvement(self, Q):
-        for i in range(Q.shape[0]):
-            for j in range(Q.shape[1]):
-                for k in range(Q.shape[2]):
-                    arg = np.argmax(Q[i,j,k])
-                    self.policy[i,j,k] = 0
-                    self.policy[i,j,k,arg] = 1
-
-        return self.policy
+def policy(state):
+    if state[0] >= 20:
+        return STICK
+    else:
+        return HIT
 
 
-def get_groud_truth(env, gamma):
-    iteration = 100
-    _, Q = algoDP.calculate_VQ_star(env, gamma, iteration)
-    return Q
+def run(env, gamma):
+
+    Value = np.zeros((22, 11, 2))   # G 的总和
+    Count = np.zeros((22, 11, 2))   # G 的数量
+
+    for episode in tqdm.trange(50000):
+        s = env.reset()
+        Episode = []     # 一幕内的(状态,动作,奖励)序列
+        done = False
+        while (done is False):            # 幕内循环
+            int_s = (s[0], s[1], int(s[2]))
+            action = policy(s)
+            next_s, reward, done, _ = env.step(action)
+            Episode.append((int_s, action, reward))
+            s = next_s  # 迭代
+
+        num_step = len(Episode)
+        G = 0
+        # 从后向前遍历计算 G 值
+        for t in range(num_step-1, -1, -1):
+            s, a, r = Episode[t]
+            G = gamma * G + r
+            Value[s] += G     # 值累加
+            Count[s] += 1     # 数量加 1
+
+    Count[Count==0] = 1 # 把分母为0的填成1，主要是针对终止状态Count为0
+    V = Value / Count   # 求均值
+    return V
+
+
 
 if __name__=="__main__":
     gamma = 1
-    rough = 1000
-    final = 20000
-    
-    np.set_printoptions(suppress=True)
     env = gym.make('Blackjack-v1', sab=True)
-    env.reset(seed=5)
-    algo = MC_Greedy(env, (0.5,0.5), gamma, rough, final)
-    Q, policy = algo.policy_iteration()
+    V = run(env, gamma)
     env.close()
-    
-    print(policy)
+    #print(V)
+
+    fig = sns.heatmap(np.flipud(V[12:,1:,0]), cmap="YlGnBu", xticklabels=range(1, 11),
+                          yticklabels=list(reversed(range(12, 22))))
+    fig.set_ylabel('player sum', fontsize=30)
+    fig.set_xlabel('dealer showing', fontsize=30)
+
+    plt.show()
 

0278674d85496f121b23c9a01e2b0472d4cb2d7c
xiaowuhu <xiaowuhu@microsoft.com>
2022-08-12 12:05:42 +0800

    update

--- 基础教程/A7-强化学习/110-二十一点问题 - 蒙特卡洛控制/src/MC_114_GLIE_Standard.py
+++ 基础教程/A7-强化学习/110-二十一点问题 - 蒙特卡洛控制/src/MC_114_GLIE_Standard.py
@@ -4,7 +4,7 @@ import Algorithm.Algo_OptimalValueFunction as algo
 import Algorithm.Base_MC_Policy_Iteration as base
 import common.CommonHelper as helper
 import common.DrawQpi as drawQ
-import tqdm
+import tqdm, math
 
 # 状态空间 = 空间宽度 x 空间高度
 GridWidth, GridHeight = 4, 4
@@ -45,43 +45,32 @@ SpecialMove = {
 # 墙
 Blocks = []
 
-class MC_SoftGreedy(base.Policy_Iteration):
-    def __init__(self, env, init_policy, gamma, epsilon):
-        super().__init__(env, init_policy, gamma)
-        self.epsilon = epsilon
-        self.best_p = 1 - epsilon + epsilon / self.nA 
-        self.other_p = epsilon / self.nA        
-
+class MC_GLIE_Standard(base.Policy_Iteration):
     def policy_improvement(self, s):
-        # 做策略改进，贪心算法
         if np.min(self.Count[s]) == 0:  # 避免被除数为 0
-            return
+            return        
+        epsilon = 1 / self.n_episode
+        other_p = epsilon / self.nA
+        best_p = 1 - epsilon + epsilon/self.nA
         self.Q[s] = self.Value[s] / self.Count[s]  # 得到该状态下所有动作的 q 值
-        self.policy[s] = self.other_p         # 先设置该状态所有策略为 epsilong/nA
+        self.policy[s] = other_p         # 先设置该状态所有策略为 epsilong/nA
         argmax = np.argmax(self.Q[s])
-        self.policy[s, argmax] = self.best_p
+        self.policy[s, argmax] = best_p
+        return self.policy
 
 
 if __name__=="__main__":
-
-    np.random.seed(15)
-
     env = model.GridWorld(
         GridWidth, GridHeight, StartStates, EndStates,  # 关于状态的参数
         Actions, Policy, Transition,                    # 关于动作的参数
         StepReward, SpecialReward,                      # 关于奖励的参数
         SpecialMove, Blocks)                            # 关于移动的限制
-    #model.print_P(env.P_S_R)
     gamma = 0.9
-    episodes = 1000
-
-    helper.print_seperator_line(helper.SeperatorLines.long, "试验探索次数")
-    epsilons = [0.1, 0.3, 0.5, 0.7]
-    
-    for epsilon in epsilons:
-        helper.print_seperator_line(helper.SeperatorLines.middle, "epsilon="+str(epsilon))
+    episodes = 2000
+    Qs = []
+    for i in range(4):
         policy = helper.create_policy(env.nS, env.nA, (0.25, 0.25, 0.25, 0.25))
-        mc = MC_SoftGreedy(env, policy, gamma, epsilon)
+        mc = MC_GLIE_Standard(env, policy, gamma)
         Q = mc.policy_iteration(episodes)
         V = helper.calculat_V_from_Q(Q, policy)
 

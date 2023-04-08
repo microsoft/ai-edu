af7d999917874a84cabdbf48a6f522d9ed3fb537
BryanSharp <8344245+hydraxman@users.noreply.github.com>
2023-04-09 00:45:49 +0800

    merge script test pass, all the md will be in output folder

--- scripts/merge_into_book.py
+++ scripts/merge_into_book.py
@@ -5,11 +5,15 @@ import shutil
 # Import the re module
 import re
 
-# Define a function named copy_img_folders that takes a folder path and a file name as parameters and copies all the img folders to the created md file folder
-def copy_img_folders(folder_path, file_name):
+# Define a function named merge_markdown_files that takes a folder path and a file name as parameters and calls copy_img_folders and merge_markdown_content functions
+def merge_markdown_files(folder_path, file_name):
+  current_root_dir = os.path.dirname(os.getcwd())
   # Use the os.walk() function to iterate over all subdirectories in the folder and store the result in a variable
   walk_result = list(os.walk(folder_path))
   # Loop through each tuple in the list
+  output_folder = "output"
+  if os.path.exists(output_folder):
+    shutil.rmtree(output_folder)
   for dirpath, dirnames, filenames in walk_result:
     # Loop through each directory name in the current subdirectory
     for dir in dirnames:
@@ -17,15 +21,15 @@ def copy_img_folders(folder_path, file_name):
       if dir == "img":
         # Get the source path of the img folder by joining it with the directory path
         src_path = os.path.join(dirpath, dir)
+        relative_path = os.path.relpath( os.path.abspath(dirpath),current_root_dir)
+        # print(f'relative_path {relative_path}')
         # Get the destination path of the img folder by joining it with the file name and removing the extension
-        dst_path = os.path.join(file_name[:-3], dir)
+        dst_path = os.path.join(output_folder,relative_path, dir)
+        # print(dst_path)
         # Copy the img folder to the destination path using shutil.copytree()
         shutil.copytree(src_path, dst_path)
-
-# Define a function named merge_markdown_content that takes a folder path and a file name as parameters and merges all the markdown files content to the new file
-def merge_markdown_content(folder_path, file_name):
   # Open a new markdown file in write mode using the with syntax
-  with open(file_name, "w") as file3:
+  with open(file_name, "w", encoding="utf-8") as file3:
     # Use the os.walk() function to iterate over all subdirectories in the folder and store the result in a variable
     walk_result = list(os.walk(folder_path))
     # Sort the list of tuples by the first element (the directory path)
@@ -36,32 +40,29 @@ def merge_markdown_content(folder_path, file_name):
       dirnames.sort()
       # Sort the list of file names in alphabetical order
       filenames.sort()
+      has_section_name = False
       # Loop through each file name in the current subdirectory
       for file in filenames:
         # Check if the file name ends with ".md"
         if file.endswith(".md"):
-          # Open the markdown file in read mode using the with syntax
-          with open(os.path.join(dirpath, file), "r") as file1:
-            # Read the content of the markdown file
-            content1 = file1.read()
+          if not has_section_name:
             # Get the folder relative path by using the os.path.relpath() function
             section_name = os.path.relpath(dirpath, folder_path)
             # Write the section name as a header, followed by a newline character
-            file3.write("# " + section_name + "\n")
+            file3.write("# " + section_name.replace("\\","\n\n# ") + "\n\n")
+            has_section_name = True
+          # Open the markdown file in read mode using the with syntax
+          with open(os.path.join(dirpath, file), "r", encoding="utf-8") as file1:
+            # Read the content of the markdown file
+            content1 = file1.read()
             # Write the file name as the title of the content, followed by a newline character
-            file3.write(file + "\n")
+            file3.write("## " +file.replace(".md","") + "\n\n")
+            relative_path = os.path.relpath(os.path.abspath(dirpath),current_root_dir)
+            # print(f'relative_path {relative_path}')
             # Replace any image path text in the content with the new location using re.sub()
-            content1 = re.sub(r"img\/\w+\.(JPG|SVG|jpg|svg)", r"![\1](file3/\2)", content1)
+            content1 = re.sub(r"img\/(\w+\.(JPG|SVG|jpg|svg))", f"{repr(relative_path)[1:-1]}/img/\g<1>", content1)
             # Write the content to the new file
-            file3.write(content1)
-
-# Define a function named merge_markdown_files that takes a folder path and a file name as parameters and calls copy_img_folders and merge_markdown_content functions
-def merge_markdown_files(folder_path, file_name):
-  # Call copy_img_folders function with folder_path and file_name as arguments
-  copy_img_folders(folder_path, file_name)
-  # Call merge_markdown_content function with folder_path and file_name as arguments
-  merge_markdown_content(folder_path, file_name)
-
+            file3.write(content1+ "\n")
 
 # Call the function with the folder path and the file name as arguments
-merge_markdown_files("../基础教程/A5-现代软件工程（更新中）", "现代软件工程.md")
+merge_markdown_files("../基础教程/A5-现代软件工程（更新中）", "output/现代软件工程.md")
